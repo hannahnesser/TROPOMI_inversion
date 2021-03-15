8512a2dc28ffb1609ae0ae4bdd6c840e91264538
Hannah Nesser <hnesser@g.harvard.edu>
2021-03-15 17:54:12 -0400

    Remove env directory

--- env/lib/python3.8/site-packages/dask/dataframe/io/parquet/__init__.py
+++
@@ -1,1 +0,0 @@
-from .core import read_parquet, to_parquet, read_parquet_part, create_metadata_file
--- env/lib/python3.8/site-packages/xesmf/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__='0.3.0'
-from . import util
-from . import data
-from . frontend import Regridder
