fe61bfae666b77b39b51fa63b1e2fdb970aae2e2
Hannah Nesser <hnesser@g.harvard.edu>
2021-03-31 23:02:34 -0400

    Removed unnecessary import statement

--- python/correct_CFL_errors.py
+++ python/correct_CFL_errors.py
@@ -8,7 +8,6 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import imageio
 
 ## ------------------------------------------------------------------------ ##
 ## Set user preferences
