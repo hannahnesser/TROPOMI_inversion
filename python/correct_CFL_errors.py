8a0b912a17ee2f59ff469255897063ba5ee562d2
Hannah Nesser <hnesser@g.harvard.edu>
2021-03-31 22:44:05 -0400

    Bug fixes to allow for seamless CFL error correction

--- python/correct_CFL_errors.py
+++ python/correct_CFL_errors.py
@@ -8,6 +8,7 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import imageio
 
 ## ------------------------------------------------------------------------ ##
 ## Set user preferences
@@ -15,8 +16,6 @@ import matplotlib.pyplot as plt
 # Local preferences
 input_dir = sys.argv[1]
 base_dir = sys.argv[2]
-# base_dir = sys.argv[1]
-# code_dir = sys.argv[2]
 data_dir = join(base_dir, 'OutputDir')
 code_dir = sys.argv[3]
 
@@ -24,10 +23,10 @@ code_dir = sys.argv[3]
 year = 2019
 months = np.arange(12, 13, 1)
 days = np.arange(1, 32, 1)
-files = [f'GEOSChem.SpeciesConc.{year}{mm:02d}{dd:02d}_0000z.nc4'
-         for mm in months for dd in days]
-files.sort()
-# profile = join(input_dir, 'default_profile.nc')
+files = join(data_dir, 'GEOSChem.SpeciesConc.YYYYMMDD_0000z.nc4')
+replacement_files = join(input_dir, 'halfstep_outputs',
+                         'GEOSChem.SpeciesConc.YYYYMMDD_0000z.nc4')
+profiles = join(input_dir, 'vertical_profiles', 'mean_profile_YYYYMM.nc')
 
 # Information on the grid # I think this is irrelevant?
 lat_bins = np.arange(10, 65, 5)
@@ -45,9 +44,6 @@ buffers = [3, 3, 3, 3]
 # Custom packages
 sys.path.append(code_dir)
 import config
-# config.SCALE = config.PRES_SCALE
-# config.BASE_WIDTH = config.PRES_WIDTH
-# config.BASE_HEIGHT = config.PRES_HEIGHT
 import gcpy as gc
 import troppy as tp
 import format_plots as fp
@@ -55,109 +51,113 @@ import format_plots as fp
 ## ------------------------------------------------------------------------ ##
 ## Replace stratosphere
 ## ------------------------------------------------------------------------ ##
-# Open default vertical profile
-profile = xr.open_dataset(profile)
-
 # Iterate through the correct files
-for i, file in enumerate(files):
-    # Open the file
-    if file not in listdir(data_dir):
-        print(f'{file} is not in the data directory.')
-        continue
-    print(f'-'*75)
-    print(f'Checking for anomalous values in {file}')
-    data = xr.open_dataset(join(data_dir, file))
+for month in months:
+    # Open the default vertical profile
+    profile = xr.open_dataset(profiles.replace('YYYYMM', f'{year}{month:02d}'))
 
-    # Check for time dimensiom
-    if len(data.time) != 24:
-        print(f'Filling in first hour.')
-        data = gc.fill_GC_first_hour(data)
+    for day in days:
+        # Get file name
+        file = files.replace('YYYYMMDD', f'{year}{month:02d}{day:02d}')
 
-    # Check if any values are more than 5x greater than or less than
-    # the profile
-    diff = np.abs(xr.ufuncs.log10(data['SpeciesConc_CH4'])/np.log10(5) -
-                  xr.ufuncs.log10(profile['SpeciesConc_CH4'])/np.log10(5))
+        # Open the file
+        if file not in listdir(data_dir):
+            print(f'{file} is not in the data directory.')
+            continue
+        print(f'-'*75)
+        print(f'Checking for anomalous values in {file}')
+        data = xr.open_dataset(join(data_dir, file))
 
-    # If so, replace those values
-    if (diff >= 1).any():
-        print(f'Replacing data in {file}')
-
-        # Check if there is a default profile
-
-
-        # Move the original file
-        os.rename(join(data_dir, file), join(data_dir, f'{file}_orig'))
-
-        # The original file, only where the problem values are
-        old = data['SpeciesConc_CH4'].where(diff >=1, drop=True).squeeze()
-
-        # Open the new file (that will replace the problem values)
-        new = xr.open_dataset(join(input_dir, file))['SpeciesConc_CH4']
-        # Check for time dimension
-        if len(new.time) != 24:
+        # Check for time dimensiom
+        if len(data.time) != 24:
             print(f'Filling in first hour.')
-            new = gc.fill_GC_first_hour(new)
+            data = gc.fill_GC_first_hour(data)
 
-        # Fill in the data by replacing the entire level
-        cond = data.lev.isin(old.lev.values)
-        data['SpeciesConc_CH4'] = data['SpeciesConc_CH4'].where(~cond, new)
+        # Check if any values are more than 5x greater than or less than
+        # the profile
+        diff = np.abs(xr.ufuncs.log10(data['SpeciesConc_CH4'])/np.log10(5) -
+                      xr.ufuncs.log10(profile['SpeciesConc_CH4'])/np.log10(5))
 
-        # Print out information
-        print(f'  Level       Old Min     New Min     Old Max     New Max')
-        for l in old.lev.values:
-            old_dat = old.sel(lev=l).values
-            new_dat = data.sel(lev=l)['SpeciesConc_CH4'].values
-            print(f'  {l:<12.2e}{np.nanmin(old_dat):<12.2e}{new_dat.min():<12.2e}{np.nanmax(old_dat):<12.2e}{new_dat.max():<12.2e}')
+        # If so, replace those values
+        if (diff >= 1).any():
+            print(f'Replacing data in {file}')
 
-        # Create a plot to check
-        data_summ = data['SpeciesConc_CH4'].mean(dim=['lat', 'lon'])
-        fig, ax = fp.get_figax()
-        ax.plot(profile['SpeciesConc_CH4'], profile.lev,
-                c=fp.color(0), lw=3)
-        ax.plot(data_summ.T, data_summ.lev, c=fp.color(4), alpha=0.5, lw=0.5)
+            # Move the original file
+            os.rename(join(data_dir, file), join(data_dir, f'{file}_orig'))
 
-        title = file.split('_')[0].split('.')[-1]
-        ax = fp.add_title(ax, f'{title}')
-        ax = fp.add_labels(ax, r'XCH4 (mol mol$^{-1}$)',
-                           'Hybrid Level at Midpoints')
-        ax.set_xlim(0, 3e-6)
-        ax.set_ylim(1, 0)
+            # The original file, only where the problem values are
+            old = data['SpeciesConc_CH4'].where(diff >=1, drop=True).squeeze()
 
-        fp.save_fig(fig, data_dir, title)
+            # Open the new file (that will replace the problem values)
+            rfile = replacement_files.replace('YYYYMMDD',
+                                              f'{year}{month:02d}{day:02d}')
+            new = xr.open_dataset(rfile)['SpeciesConc_CH4']
+            # Check for time dimension
+            if len(new.time) != 24:
+                print(f'Filling in first hour.')
+                new = gc.fill_GC_first_hour(new)
 
-        # save out file
-        data.to_netcdf(join(data_dir, file))
+            # Fill in the data by replacing the entire level
+            cond = data.lev.isin(old.lev.values)
+            data['SpeciesConc_CH4'] = data['SpeciesConc_CH4'].where(~cond, new)
 
-        # # Optional plotting
-        # for t in data.time:
-        #     datas = data['SpeciesConc_CH4']
-        #     ds = datas.sel(time=t)
-        #     for i, l in enumerate(ds.lev):
-        #         if l < 0.3:
-        #             new_title = f'{title}-{i:02d}'
-        #             if f'{new_title}.png' not in listdir(data_dir):
-        #                 datas = ds.sel(lev=l)
-        #                 fig, ax = fp.get_figax(maps=True, lats=datas.lat, lons=datas.lon)
-        #                 ax = fp.format_map(ax, lats=datas.lat, lons=datas.lon)
-        #                 c = datas.plot(ax=ax, cmap=fp.cmap_trans('viridis'),
-        #                               add_colorbar=False, vmin=0, vmax=0.0000025)
+            # Print out information
+            print(f'  Level       Old Min     New Min     Old Max     New Max')
+            for l in old.lev.values:
+                old_dat = old.sel(lev=l).values
+                new_dat = data.sel(lev=l)['SpeciesConc_CH4'].values
+                print(f'  {l:<12.2e}{np.nanmin(old_dat):<12.2e}{new_dat.min():<12.2e}{np.nanmax(old_dat):<12.2e}{new_dat.max():<12.2e}')
 
-        #                 # Add colorbar
-        #                 cax = fp.add_cax(fig, ax)
-        #                 cb = fig.colorbar(c, cax=cax)
-        #                 cb = fp.format_cbar(cb, cbar_title='')
+            # Create a plot to check
+            data_summ = data['SpeciesConc_CH4'].mean(dim=['lat', 'lon'])
+            fig, ax = fp.get_figax()
+            ax.plot(profile['SpeciesConc_CH4'], profile.lev,
+                    c=fp.color(0), lw=3)
+            ax.plot(data_summ.T, data_summ.lev, c=fp.color(4),
+                    alpha=0.5, lw=0.5)
 
-        #                 ax = fp.add_title(ax, f'{new_title}')
-        #                 fp.save_fig(fig, data_dir, f'{new_title}')
-        #                 plt.close()
+            title = file.split('_')[0].split('.')[-1]
+            ax = fp.add_title(ax, f'{title}')
+            ax = fp.add_labels(ax, r'XCH4 (mol mol$^{-1}$)',
+                               'Hybrid Level at Midpoints')
+            ax.set_xlim(0, 3e-6)
+            ax.set_ylim(1, 0)
 
-        # # Create a gif
-        # images = []
-        # files = [f for f in listdir(data_dir) if (f[-3:] == 'png') and (len(f) == 15)]
-        # files.sort()
-        # # files = files[::-1]
-        # print(files)
-        # for f in files:
-        #     images.append(imageio.imread(join(data_dir, f)))
-        # imageio.mimsave(join(data_dir, f'{title}.gif'),
-        #                 images, duration=0.5)
+            fp.save_fig(fig, data_dir, title)
+
+            # save out file
+            data.to_netcdf(join(data_dir, file))
+
+            # # Optional plotting
+            # for t in data.time:
+            #     datas = data['SpeciesConc_CH4']
+            #     ds = datas.sel(time=t)
+            #     for i, l in enumerate(ds.lev):
+            #         if l < 0.3:
+            #             new_title = f'{title}-{i:02d}'
+            #             if f'{new_title}.png' not in listdir(data_dir):
+            #                 datas = ds.sel(lev=l)
+            #                 fig, ax = fp.get_figax(maps=True, lats=datas.lat, lons=datas.lon)
+            #                 ax = fp.format_map(ax, lats=datas.lat, lons=datas.lon)
+            #                 c = datas.plot(ax=ax, cmap=fp.cmap_trans('viridis'),
+            #                               add_colorbar=False, vmin=0, vmax=0.0000025)
+
+            #                 # Add colorbar
+            #                 cax = fp.add_cax(fig, ax)
+            #                 cb = fig.colorbar(c, cax=cax)
+            #                 cb = fp.format_cbar(cb, cbar_title='')
+
+            #                 ax = fp.add_title(ax, f'{new_title}')
+            #                 fp.save_fig(fig, data_dir, f'{new_title}')
+            #                 plt.close()
+
+            # # Create a gif
+            # images = []
+            # files = [f for f in listdir(data_dir) if (f[-3:] == 'png') and (len(f) == 15)]
+            # files.sort()
+            # # files = files[::-1]
+            # print(files)
+            # for f in files:
+            #     images.append(imageio.imread(join(data_dir, f)))
+            # imageio.mimsave(join(data_dir, f'{title}.gif'),
+            #                 images, duration=0.5)
--- scripts/generate_obs.sh
+++ scripts/generate_obs.sh
@@ -3,7 +3,7 @@
 # User settings
 PRIOR_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TROPOMI_inversion_0000/"
 TROPOMI_DIR="/n/seasasfs02/CH4_inversion/InputData/Obs/TROPOMI/"
-HALFSTEP_DIR="/n/seasasfs02/hnesser/TROPOMI_inversion/gc_outputs/halfstep_outputs"
+CORRECT_CFL_DIR="/n/seasasfs02/hnesser/TROPOMI_inversion/gc_outputs/"
 CODE_DIR="/n/home04/hnesser/TROPOMI_inversion/python"
 
 # Latitude and longitude range
@@ -16,11 +16,12 @@ YEAR="2019"
 # MONTH= months are set in the array variable
 
 # Check for unphysical stratospheric values
-jid1=$(sbatch correct_CFL_errors.sh ${PRIOR_DIR} ${HALFSTEP_DIR} ${CODE_DIR})
+jid1=$(sbatch correct_CFL_errors.sh ${PRIOR_DIR} ${CORRECT_CFL_DIR} ${CODE_DIR})
+mv ${PRIOR_DIR}/OutputDir/*_
 
 # Apply the TROPOMI operator
-jid2=$(sbatch --dependency=afterok:${jid1##* } --array=1-12 apply_TROPOMI_operator.sh ${TROPOMI_DIR} ${PRIOR_DIR})
+jid2=$(sbatch --dependency=afterok:${jid##* } --array=1-12 apply_TROPOMI_operator.sh ${TROPOMI_DIR} ${PRIOR_DIR})
 
 # Analyze the output
-sbatch --dependency=afterok:${jid2##* } run_generate_obs.sh ${PRIOR_DIR} ${CODE_DIR}
+sbatch --dependency=afterok:${jid##* } run_generate_obs.sh ${PRIOR_DIR} ${CODE_DIR}
 # sbatch run_generate_obs.sh ${PRIOR_DIR} ${CODE_DIR}
