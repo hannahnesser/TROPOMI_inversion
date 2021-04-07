836739ac23f817af4862b451a56d9e5a6fb8787a
Hannah Nesser <hnesser@g.harvard.edu>
2021-04-06 23:28:17 -0400

    Added a file containing default inversion settings

---
+++ python/inversion_settings.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('.')
+import gcpy as gc
+
+# Time settings
+year = 2019
+months = [i for i in range(1, 13)]
+days = [i for i in range(1, 32, 1)]
+
+# Information on the grid
+lat_min = 9.75
+lat_max = 60
+lat_delta = 0.25
+
+lon_min = -130
+lon_max = -60
+lon_delta = 0.3125
+
+buffers = [3, 3, 3, 3]
+
+# Adjust for buffer cells
+lats, lons = gc.adjust_grid_bounds(lat_min, lat_max, lat_delta,
+                                   lon_min, lon_max, lon_delta, buffers)
+lat_min, lat_max = lats
+lon_min, lon_max = lons
