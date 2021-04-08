f4c5a2a5f482a72d30c0bcf9c8e310877205d7e3
Hannah Nesser <hnesser@g.harvard.edu>
2021-04-08 10:55:23 -0400

    Renamed k0 generating scripts

---
+++ scripts/generate_k0_monthly.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH -J save_k0
+#SBATCH -o %x_%j_%a.out
+#SBATCH -c 15
+#SBATCH -N 1
+#SBATCH -p huce_intel
+#SBATCH --mem 55000
+#SBATCH -t 0-00:30
+#SBATCH --mail-type=END
+
+## -------------------------------------------------------------------------##
+## Set user preferences
+## -------------------------------------------------------------------------##
+YEAR="2019"
+MONTH="${SLURM_ARRAY_TASK_ID}"
+export OMP_NUM_THREADS=6
+
+## -------------------------------------------------------------------------##
+## Load the environment
+## -------------------------------------------------------------------------##
+echo "Activating python environment"
+
+module load Anaconda3/5.0.1-fasrc01
+source activate ~/python/miniconda/envs/TROPOMI_inversion
+
+echo "Activated ${CONDA_PREFIX}"
+
+## -------------------------------------------------------------------------##
+## Run the script
+## -------------------------------------------------------------------------##
+echo "Initiating script"
+
+python_dir=$(dirname `pwd`)
+python -u ${python_dir}/python/generate_k0.py $MONTH
