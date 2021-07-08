fa3ad9dc17bd123a4e21fd00722539e198dab25e
Hannah Nesser <hnesser@g.harvard.edu>
2021-07-08 15:08:12 -0400

    Modified default output dir

--- scripts/generate_obs.sh
+++ scripts/generate_obs.sh
@@ -5,6 +5,7 @@ PRIOR_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TR
 TROPOMI_DIR="/n/seasasfs02/CH4_inversion/InputData/Obs/TROPOMI/"
 CORRECT_CFL_DIR="/n/seasasfs02/hnesser/TROPOMI_inversion/gc_outputs/"
 CODE_DIR="/n/home04/hnesser/TROPOMI_inversion/python"
+OUTPUT_DIR="/n/seasasfs02/hnesser/TROPOMI_inversion/inversion_data"
 
 # Latitude and longitude range
 LONS="-130 -60 0.3125"
@@ -22,5 +23,5 @@ jid1=$(sbatch preprocess_GC_output.sh ${PRIOR_DIR} ${CORRECT_CFL_DIR} ${CODE_DIR
 jid2=$(sbatch --dependency=afterok:${jid1##* } --array=1-12 apply_TROPOMI_operator.sh ${TROPOMI_DIR} ${PRIOR_DIR})
 
 # Analyze the output
-sbatch --dependency=afterok:${jid2##* } run_generate_obs.sh ${PRIOR_DIR} ${CODE_DIR}
+sbatch --dependency=afterok:${jid2##* } run_generate_obs.sh ${PRIOR_DIR} ${OUTPUT_DIR} ${CODE_DIR}
 # sbatch run_generate_obs.sh ${PRIOR_DIR} ${CODE_DIR}
