eef6091694827a0fc1dcc3dc1aaccc662ccf3937
Hannah Nesser <hnesser@g.harvard.edu>
2021-07-08 15:25:30 -0400

    Increasing time request

--- scripts/preprocess_GC_output.sh
+++ scripts/preprocess_GC_output.sh
@@ -6,7 +6,7 @@
 #SBATCH -N 1
 #SBATCH -p huce_intel
 #SBATCH --mem 4000
-#SBATCH -t 0-00:30
+#SBATCH -t 0-01:00
 #SBATCH --mail-type=END
 
 ## -------------------------------------------------------------------------##
