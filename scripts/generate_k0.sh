d9be5cf61b5c18dd206f3ec3e373f902deadc5c9
Hannah Nesser <hnesser@g.harvard.edu>
2021-07-12 15:35:01 -0400

    Debugging monthly  jacobian construction

--- scripts/generate_k0.sh
+++ scripts/generate_k0.sh
@@ -3,3 +3,6 @@
 jid1=$(sbatch generate_k0_nstate.sh)
 
 sbatch --dependency=afterok:${jid1##* } --array=1-12 generate_k0_monthly.sh
+
+sbatch --array=1-12 generate_k0_monthly.sh
+sbatch --array=1 generate_k0_monthly.sh
