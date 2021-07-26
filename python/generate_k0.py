650e1b7822bb2ea7c1b61eb887f2e5d51731151c
Hannah Nesser <hnesser@g.harvard.edu>
2021-07-25 21:50:54 -0500

    Reducing to only three months as an initial test

--- python/generate_k0.py
+++ python/generate_k0.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     ## Generate a monthly K0
     ## -------------------------------------------------------------------- ##
     # Iterate through the months
-    for m in s.months:
+    for m in range(1, 3):#s.months:
         print('-'*75)
         print(f'Month {m}')
 
