838052e6eabaa1d76c475dec382e2c6fff748e12
Hannah Nesser <hnesser@g.harvard.edu>
2021-07-26 12:08:50 -0500

    Many changes to allow for the generate_k0.sh script to be seamless

--- scripts/generate_k0.sh
+++ scripts/generate_k0.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-jid1=$(sbatch generate_k0_nstate.sh)
+# Directories
+LONG_TERM_DATA_DIR="/n/seasasfs02/hnesser/TROPOMI_inversion/"
+SHORT_TERM_DATA_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/initial_inversion/"
+CODE_DIR="/n/home04/hnesser/TROPOMI_inversion/python/"
 
-sbatch --dependency=afterok:${jid1##* } --array=1-12 generate_k0_monthly.sh
+# First, generate the nstate x nstate x months first guess of the Jacobian.
+# Note that there are some directory relationships that are hard-coded here--
+# so be careful about changes to directory structures.
+# Command structure:
+# sbatch generate_k0_nstate data_dir output_dir code_dir
+jid1=$(sbatch generate_k0_nstate.sh ${LONG_TERM_DATA_DIR} ${SHORT_TERM_DATA_DIR} ${CODE_DIR})
 
-sbatch generate_k0_monthly.sh
+# Second, generate the first guess of the m x n Jacobian on a monthly basis
+# Command structure:
+# sbatch generate_k0_monthly.sh data_dir output_dir code_dir
+jid2=$(sbatch --dependency=afterok:${jid1##* } generate_k0_monthly.sh ${LONG_TERM_DATA_DIR} ${SHORT_TERM_DATA_DIR} ${CODE_DIR})
