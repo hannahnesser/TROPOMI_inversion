61f32b5e0724f7876f9e16ed25c9df4dc3d00817
Hannah Nesser <hnesser@g.harvard.edu>
2021-08-16 19:17:30 -0400

    Many changes preparing for EM update of 8/2021

--- python/boundary_condition_sensitivity.py
+++ python/boundary_condition_sensitivity.py
@@ -15,6 +15,13 @@ import format_plots as fp
 
 np.set_printoptions(precision=3, linewidth=150)
 
+# NOTE: This is all hard coded to be a relative inversion.
+
+## -------------------------------------------------------------------------##
+# File Locations
+## -------------------------------------------------------------------------##
+plot_dir = '../plots/BC_sensitivity'
+
 ## -------------------------------------------------------------------------##
 # Define the model parameters
 ## -------------------------------------------------------------------------##
@@ -30,12 +37,14 @@ tau = 1/j
 print('Lifetime (hrs): ', tau/3600)
 
 # Dimensions of the inversion quantities
-nstate = 20 # state vector
+nstate = 30 # state vector
 nobs_per_cell = 30
 nobs = nobs_per_cell*nstate # observation vector
 
 # Define the times at which we sample the forward model
-t = np.arange(0, (nobs_per_cell+1)*0.9*L/U, 0.9*L/U)
+C = 0.5 # Courant number
+delta_t = C*L/U
+t = np.arange(0, (nobs_per_cell+1)*delta_t, delta_t)
 print('Times (hrs): ', t/3600)
 
 # Define the true and prior emissions, including the boundary conditions
@@ -43,12 +52,16 @@ BC_true = 1900 # ppb
 x_true = 100*np.ones(nstate)/(3600*24) # true (ppb/s)
 x_a = rs.normal(loc=80, scale=30,
                 size=(nstate,))/(3600*24) # prior (ppb/s)
+# x_a[x_a*3600*24 < 50] = 50/(3600*24)
 print('Emissions (ppb/hr):')
 print('  True:  ', x_true*3600)
 print('  Prior: ', x_a*3600)
 
-# Define the prior error
-s_a = 0.25*np.identity(nstate)
+# Define the prior error (This is n+1 x n+1 to accomodate BC optimization)
+s_a = 0.5**2*np.identity(nstate)
+idx = x_a < 0.5*x_a.mean()
+# idx = np.append(False, idx)
+s_a[idx, idx] = 0.5*x_a.mean()/x_a[idx]
 
 # Define steady state concentrations
 # These will be both our initial condition and serve as the starting
@@ -64,71 +77,16 @@ print(y_ss, '\n')
 y_init = copy.deepcopy(y_ss)
 
 # Create pseudo-observations
-random_noise = rs.normal(-3.4, 5.6, (nstate, nobs_per_cell))
+random_noise = rs.normal(0, 10, (nstate, nobs_per_cell))
 y = y_ss.reshape(-1,1) + random_noise
 # y = y.flatten()
 
 # Define the observational errror
-s_o = 4**2*np.identity(nobs)
+s_o = 15**2*np.identity(nobs)
 
 ## -------------------------------------------------------------------------##
 # Define forward model and inversion functions
 ## -------------------------------------------------------------------------##
-# BC needs to be input as k*BC
-def forward_model(x, y_init, BC, J, time):
-    '''
-    A function that calculates the mass in each reservoir
-    after a given time given the following:
-        x       :    vector of emissions
-        y_init  :    initial atmospheric condition
-        BC      :    boundary condition
-        J       :    matrix of first-order, time-invariant
-                     rate coefficients kiJ describing the
-                     flow from reservoir i to reservoir J
-        time    :    time to run the forward model (either a
-                     scalar or a vector)    '''
-    # Define the source (rows correspond to the reservoir
-    # and columns to the times)
-    s = copy.deepcopy(x).reshape((-1, 1))
-
-    # Add the contribution from the boundary condition
-    if isinstance(BC, np.ndarray):
-        assert len(BC) == len(time), 'Varying BC must match time dimension.'
-        s = np.tile(s, len(time))
-        s[0, :] = s[0, :] + BC.reshape((1, -1))
-    else:
-        s[0] += BC
-        s = s.reshape((-1, 1))
-
-    # Conduct eigendecomposition of J
-    w, v = np.linalg.eig(J)
-    w = w.reshape((-1, 1))
-
-    # The rows will correspond to each reservoir
-    # and the columns will correspond to each time.
-    # Check these dimensions now.
-    if ~isinstance(time, np.ndarray):
-        time = np.array([time]).reshape((1, -1))
-    y_init = y_init.reshape((-1, 1))
-    # s = s.reshape((-1, 1))
-
-    # dy/dt = Jy + x + BC is not a homogeneous differential
-    # equation. We calculate a particular and a homogeneous
-    # solution. The particular solution will be of the form
-    # m(t) = a*t + b:
-    a = np.linalg.solve(J, np.zeros(y_init.shape))
-    b = np.linalg.solve(J, a - s)
-    y_partic = a*time + b
-    # Note: if the source is 0, mass_partic is also 0
-
-    # Then, the homogenous solution will be calculated
-    # following Amos et al. (2013)
-    alphas = np.linalg.solve(v, y_init - b)
-    y_homog = v @ (alphas*np.exp(w*time))
-
-    # The final mass is the sum of the two solutions
-    return y_homog + y_partic
-
 def forward_model_lw(x, y_init, BC, times, U, L):
     '''
     A function that calculates the mass in each reservoir
@@ -140,7 +98,7 @@ def forward_model_lw(x, y_init, BC, times, U, L):
         U       :    wind speed
         L.      :    length scale for each grid box
     '''
-    # Create an empty array (time x grid box) for the output
+    # Create an empty array (grid box x time) for the output
     ys = np.zeros((len(y_init), len(times)))
     ys[:, 0] = y_init
 
@@ -153,6 +111,8 @@ def forward_model_lw(x, y_init, BC, times, U, L):
             bc = BC
         y_new = do_emissions(x, ys[:, i], dt)
         ys[:, i+1] = do_advection(x, y_new, bc, dt, U, L)
+        # y_new = do_advection(x, ys[:, i], bc, dt, U, L)
+        # ys[:, i+1] = do_emissions(x, y_new, dt)
     return ys[:, 1:]
 
 def do_emissions(x, y_prev, delta_t):
@@ -167,31 +127,29 @@ def do_advection(x, y_prev, BC, delta_t, U, L):
     C = U*delta_t/L
 
     # Append the boundary conditions
-    try:
-        LBC = BC[0]
-        RBC = BC[1]
-    except:
-        LBC = BC
-        RBC = BC
-    y_prev = np.append(LBC, y_prev)
-    y_prev = np.append(y_prev, RBC)
+    y_prev = np.append(BC, y_prev)
 
-    # Calculate the next time step
+    # Calculate the next time step using Lax-Wendroff
     y_new = (y_prev[1:-1]
              - C*(y_prev[2:] - y_prev[:-2])/2
              + C**2*(y_prev[2:] - 2*y_prev[1:-1] + y_prev[:-2])/2)
 
+    # Update the last grid cell using upstream
+    y_new = np.append(y_new, y_prev[-1] - C*(y_prev[-1] - y_prev[-2]))
+
     return y_new
 
-def build_jacobian(x_a, y_init, BC, times, U, L):
+def build_jacobian(x_a, y_init, BC, times, U, L, optimize_BC=False):
     F = lambda x : forward_model_lw(x=x, y_init=y_init, BC=BC,
                                     times=times, U=U, L=L).flatten()
 
     # Calculate prior observations
     y_a = F(x_a)
 
-    # Iterate through the state vector elements
+    # Initialize the Jacobian
     K = np.zeros((len(y_a), len(x_a)))
+
+    # Iterate through the state vector elements
     for i in range(len(x_a)):
         # Apply the perturbation to the ith state vector element
         x = copy.deepcopy(x_a)
@@ -203,28 +161,51 @@ def build_jacobian(x_a, y_init, BC, times, U, L):
         # Save out the result
         K[:, i] = (y_pert - y_a)/0.5
 
+    if optimize_BC:
+        # Add a column for the optimization of the boundary condition
+        y_pert = forward_model_lw(x=x_a, y_init=y_init, BC=1.5*BC,
+                                  times=times, U=U, L=L).flatten()
+        dy_dx = ((y_pert - y_a)/0.5).reshape(-1, 1)
+        K = np.append(dy_dx, K, axis=1)
+
     return K
 
-def solve_inversion(x_a, s_a, y, y_a, s_o, k):
-    x_a = np.ones(nstate)
+def solve_inversion(x_a, s_a, y, y_a, s_o, k, optimize_BC=False):
+    if optimize_BC:
+        n = nstate + 1
+    else:
+        n = nstate
+
+    x_a = np.ones(n)
     c = y_a - k @ x_a
     s_hat = np.linalg.inv(np.linalg.inv(s_a) + k.T @ np.linalg.inv(s_o) @ k)
-    a = np.identity(nstate) - s_hat @ np.linalg.inv(s_a)
+    a = np.identity(n) - s_hat @ np.linalg.inv(s_a)
     x_hat = (x_a
              + s_hat @ k.T @ np.linalg.inv(s_o) @ (y - k @ x_a - c))
     return x_hat, s_hat, a
 
-def plot_inversion(x_a, x_hat, x_true):
+def plot_inversion(x_a, x_hat, x_true, x_hat_true=None):
     fig, ax = format_plot(nstate)
 
     xp = np.arange(1, nstate+1)
-    ax.plot(xp, 3600*24*x_a, c=fp.color(5), marker='.', markersize=10,
+    ax.plot(xp, 3600*24*x_true, c=fp.color(2), ls='--', label='Truth')
+    ax.plot(xp, 3600*24*x_a, c=fp.color(4), marker='.', markersize=10,
             label='Prior')
-    ax.plot(xp, 3600*24*x_a*x_hat, c=fp.color(8), marker='*', markersize=10,
-               label='Posterior')
-    ax.plot(xp, 3600*24*x_true, c=fp.color(3), ls='--', label='Truth')
-    ax = fp.add_legend(ax, bbox_to_anchor=(0.5, 1.05), loc='lower center',
-                       ncol=3)
+    if x_hat_true is not None:
+        ax.plot(xp, 3600*24*x_hat_true*x_a, c=fp.color(6),
+                marker='*', markersize=10, label='True BC Posterior')
+        ax.plot(xp, 3600*24*x_a*x_hat, c=fp.color(8), marker='.', markersize=5,
+                lw=1, label='Posterior')
+        ncol = 2
+    else: # if x_hat_true is none
+        ax.plot(xp, 3600*24*x_a*x_hat, c=fp.color(6), marker='*', markersize=10,
+               label='True BC Posterior')
+
+        ncol = 3
+
+    # Aesthetics
+    ax = fp.add_legend(ax, bbox_to_anchor=(0.5, -0.35), loc='upper center',
+                       ncol=ncol)
     ax = fp.add_labels(ax, 'State Vector Element', 'Emissions (ppb/day)')
     ax.set_ylim(0, 200)
 
@@ -235,16 +216,25 @@ def plot_obs(nstate, y, y_a, y_ss):
     fig, ax = format_plot(nstate)
 
     xp = np.arange(1, nstate+1)
-    ax.plot(xp, y_a, c=fp.color(3), lw=1, ls=':',
-            label='Modeled')
-    ax.plot(xp, y, c=fp.color(5), lw=1, marker='.', markersize=10,
-            label='Observed')
-    ax.plot(xp, y_ss, c=fp.color(8), lw=3,
-            label='Steady State')
-    # ax = fp.add_legend(ax, bbox_to_anchor=(0.5, 1.05), loc='lower center',
-    #                    ncol=3)
+    ax.plot(xp, y_ss, c='black', lw=2,
+            label='Steady State', zorder=10)
+    ax.plot(xp, y, c='grey', lw=0.5, ls=':', markersize=10,
+            label='Observed', zorder=9)
+    for i, y_a_column in enumerate(y_a.T):
+        if (i == 0) or (i == (len(t) - 2)):
+            ax.plot(xp, y_a_column,
+                    c=fp.color(i+2, cmap='plasma', lut=len(t+2)),
+                    lw=0.5, ls='-', label=f'Modeled (t={(t[i+1]/3600)} hrs)')
+        else:
+            ax.plot(xp, y_a_column,
+                    c=fp.color(i+2, cmap='plasma', lut=len(t+2)),
+                    lw=0.5, ls='-')
+
+    # Aesthetics
+    ax = fp.add_legend(ax, bbox_to_anchor=(0.5, -0.35), loc='upper center',
+                       ncol=2)
     ax = fp.add_labels(ax, 'State Vector Element', 'XCH4 (ppb)')
-    ax.set_ylim(1800, 2100)
+    ax.set_ylim(1800, 2400)
 
     return fig, ax
 
@@ -253,66 +243,101 @@ def format_plot(nstate):
     for i in range(nstate+2):
         ax.axvline(i-0.5, c=fp.color(1), alpha=0.2, ls=':')
     ax.set_xlim(0.5, nstate+0.5)
+    ax.set_facecolor('white')
     return fig, ax
 
 ## -------------------------------------------------------------------------##
 # Solve the inversion with a variety of boundary conditions
 ## -------------------------------------------------------------------------##
+# Inversion plots
+xp = np.arange(1, nstate+1)
+
 # Test 1: BC = constant (1900)
-BC_a = BC_true
-y_a = forward_model_lw(x_a, y_init, BC_a, t, U, L)#.flatten()
-K = build_jacobian(x_a, y_init, BC_a, t, U, L)
-x_hat, s_hat, a = solve_inversion(x_a, s_a, y.flatten(), y_a.flatten(), s_o, K)
+y_a = forward_model_lw(x_a, y_init, BC_true, t, U, L)#.flatten()
+K = build_jacobian(x_a, y_init, BC_true, t, U, L)
+x_hat_true, s_hat, a = solve_inversion(x_a, s_a,
+                                       y.flatten(), y_a.flatten(), s_o, K)
+# x_hat_true = x_hat_true[1:]
 
-fig, ax = plot_inversion(x_a, x_hat, x_true)
-fp.save_fig(fig, '../plots/', f'constant_BC_1900_n{nstate}_m{nobs}')
+fig, ax = plot_inversion(x_a, x_hat_true, x_true)
+ax = fp.add_title(ax, f'True Boundary Condition\n(BC = {BC_true:d} ppb)')
+fp.save_fig(fig, plot_dir, f'constant_BC_{BC_true:d}_n{nstate}_m{nobs}')
+
 fig, ax = plot_obs(nstate, y, y_a, y_ss)
-fp.save_fig(fig, '../plots', f'constant_BC_1900_n{nstate}_m{nobs}_obs')
+ax = fp.add_title(ax, f'True Boundary Condition\n(BC = {BC_true:d} ppb)')
+fp.save_fig(fig, plot_dir, f'constant_BC_{BC_true:d}_n{nstate}_m{nobs}_obs')
+plt.close()
 
-# Test 2: constant BC = BC_true - 200 ppb
-BC_a = BC_true - 200
-K = build_jacobian(x_a, y_init, BC_a, t, U, L)
-y_a = forward_model_lw(x_a, y_init, BC_a, t, U, L)
-x_hat, s_hat, a = solve_inversion(x_a, s_a, y.flatten(), y_a.flatten(), s_o, K)
+# Test 2: constant BC perturbation
+perts = [50, 100, 200, 300, 400]
+fig_summ, ax_summ = format_plot(nstate)
+for i, pert in enumerate(perts):
+    BC = BC_true + pert
 
-fig, ax = plot_inversion(x_a, x_hat, x_true)
-fp.save_fig(fig, '../plots/', f'constant_BC_1700_n{nstate}_m{nobs}')
-fig, ax = plot_obs(nstate, y, y_a, y_ss)
-fp.save_fig(fig, '../plots', f'constant_BC_1700_n{nstate}_m{nobs}_obs')
+    # Solve inversion
+    K = build_jacobian(x_a, y_init, BC, t, U, L)
+    y_a = forward_model_lw(x_a, y_init, BC, t, U, L)
+    x_hat, s_hat, a = solve_inversion(x_a, s_a,
+                                      y.flatten(), y_a.flatten(), s_o, K)
+    # x_hat = x_hat[1:]
 
+    fig, ax = plot_inversion(x_a, x_hat, x_true, x_hat_true)
+    ax = fp.add_title(ax, f'High Boundary Condition\n(BC = {BC:d} ppb)')
+    fp.save_fig(fig, plot_dir, f'constant_BC_{BC:d}_n{nstate}_m{nobs}')
 
-# Test 3: constant BC = BC_true + 200 ppb
-BC_a = BC_true + 200
-K = build_jacobian(x_a, y_init, BC_a, t, U, L)
-y_a = forward_model_lw(x_a, y_init, BC_a, t, U, L)
-x_hat, s_hat, a = solve_inversion(x_a, s_a, y.flatten(), y_a.flatten(), s_o, K)
+    fig, ax = plot_obs(nstate, y, y_a, y_ss)
+    ax = fp.add_title(ax, f'High Boundary Condition\n(BC = {BC:d} ppb)')
+    fp.save_fig(fig, plot_dir, f'constant_BC_{BC:d}_n{nstate}_m{nobs}_obs')
 
-fig, ax = plot_inversion(x_a, x_hat, x_true)
-fp.save_fig(fig, '../plots/', f'constant_BC_2100_n{nstate}_m{nobs}')
-fig, ax = plot_obs(nstate, y, y_a, y_ss)
-fp.save_fig(fig, '../plots', f'constant_BC_2100_n{nstate}_m{nobs}_obs')
+    ax_summ.plot(xp, np.abs(x_hat - x_hat_true)*x_a*3600*24,
+                 c=fp.color(k=2*i), lw=2, ls='-',
+                 label=f'{pert:d}/-{pert:d} ppb')
 
+fp.add_title(ax_summ, 'Constant Boundary Condition Perturbations')
+fp.add_labels(ax_summ, 'State Vector Element', r'$\Delta$XCH4 (ppb)')
+fp.add_legend(ax_summ, bbox_to_anchor=(0.5, -0.35), loc='upper center', ncol=3)
+fp.save_fig(fig_summ, plot_dir, f'constant_BC_summary_n{nstate}_m{nobs}')
+plt.close()
 
-# Test 4: BC = BC_true
-BC_a = [BC_true, y_ss[-1]]
-y_a = forward_model_lw(x_a, y_init, BC_a, t, U, L)#.flatten()
-K = build_jacobian(x_a, y_init, BC_a, t, U, L)
-x_hat, s_hat, a = solve_inversion(x_a, s_a, y.flatten(), y_a.flatten(), s_o, K)
+# Test 3: oscillating BC perturbation
+# Summary plot
+fig_perts, ax_perts = fp.get_figax(aspect=5)
+fig_summ, ax_summ = format_plot(nstate)
+BC1 = 2000 - 200*np.cos(2*np.pi/t.max()*t)
+BC2 = 2000 + 200*np.cos(2*np.pi/t.max()*t)
+BC3 = 2000 - 100*np.cos(2*np.pi/t.max()*t)
+BC4 = 2000 - 200*np.cos(4*np.pi/t.max()*t)
+BC5 = 2000 - 200*np.cos(6*np.pi/t.max()*t)
+BCs = [BC1, BC2, BC3, BC4, BC5]
+for i, BC in enumerate(BCs):
+    # Summary plot
+    ax_perts.plot(t/3600, BC, c=fp.color(i*2, lut=len(BCs)*2), lw=2)
 
-fig, ax = plot_inversion(x_a, x_hat, x_true)
-fp.save_fig(fig, '../plots/', f'true_BC_n{nstate}_m{nobs}')
-fig, ax = plot_obs(nstate, y, y_a, y_ss)
-fp.save_fig(fig, '../plots', f'true_BC_n{nstate}_m{nobs}_obs')
+    # Solve inversion
+    K = build_jacobian(x_a, y_init, BC, t, U, L)
+    y_a = forward_model_lw(x_a, y_init, BC, t, U, L)
+    x_hat, s_hat, a = solve_inversion(x_a, s_a,
+                                      y.flatten(), y_a.flatten(), s_o, K)
+    # x_hat = x_hat[1:]
 
-# Test 5: Oscillating BC
-BC_a = 2000 - 200*np.sin(2*np.pi/t.max()*t)
-K = build_jacobian(x_a, y_init, BC_a, t, U, L)
-y_a = forward_model_lw(x_a, y_init, BC_a, t, U, L)
-x_hat, s_hat, a = solve_inversion(x_a, s_a, y.flatten(), y_a.flatten(), s_o, K)
+    # Plots
+    fig, ax = plot_inversion(x_a, x_hat, x_true, x_hat_true)
+    fp.save_fig(fig, plot_dir, f'oscillating_BC{i}_n{nstate}_m{nobs}')
+    fig, ax = plot_obs(nstate, y, y_a, y_ss)
+    fp.save_fig(fig, plot_dir, f'oscillating_BC{i}_n{nstate}_m{nobs}_obs')
 
-fig, ax = plot_inversion(x_a, x_hat, x_true)
-fp.save_fig(fig, '../plots/', f'oscillating_BC_n{nstate}_m{nobs}')
-fig, ax = plot_obs(nstate, y, y_a, y_ss)
-fp.save_fig(fig, '../plots', f'oscillating_BC_n{nstate}_m{nobs}_obs')
+    # Summary plot
+    ax_summ.plot(xp, np.abs(x_hat - x_hat_true)*x_a*3600*24,
+                 c=fp.color(i*2, lut=len(BCs)*2), lw=2, ls='-',
+                 label=f'Test {i+1}')
 
+ax_perts.set_xlim(t.min()/3600, t.max()/3600)
+ax_perts.set_ylim(1600, 2300)
+ax_perts = fp.add_labels(ax_perts, 'Time (hr)', 'BC (ppb)')
+fp.save_fig(fig_perts, plot_dir, f'oscillating_BC_perts_summary')
 
+fp.add_title(ax_summ, 'Oscillating Boundary Condition Perturbations')
+fp.add_labels(ax_summ, 'State Vector Element', r'$\Delta$XCH4 (ppb)')
+fp.add_legend(ax_summ, bbox_to_anchor=(0.5, -0.35), loc='upper center', ncol=3)
+fp.save_fig(fig_summ, plot_dir, f'oscillating_BC_summary_n{nstate}_m{nobs}')
+plt.close()
