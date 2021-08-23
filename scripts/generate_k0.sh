0b537cd1e690e1b7f8dbb864ee4b676c52b3694f
Hannah Nesser <hnesser@g.harvard.edu>
2021-08-23 17:12:17 -0400

    Changes to add initial eigenvector calculation to basic workflow for generate_k0.sh

--- scripts/generate_k0.sh
+++ scripts/generate_k0.sh
@@ -25,4 +25,4 @@ jid3=$(sbatch --dependency=afterok:${jid2##* } --array=1-12 generate_pph0.sh ${S
 # sbatch --array=10 generate_pph0.sh ${SHORT_TERM_DATA_DIR} ${CODE_DIR}
 
 # Fourth and finally, calculate the eigenvectors
-
+jid4=$(sbatch --dependency=afterok:${jid3##* } generate_evecs0.sh ${SHORT_TERM_DATA_DIR} ${CODE_DIR})
