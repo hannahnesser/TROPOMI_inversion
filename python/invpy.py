9a2223704646bf094ff6f8f072479b92191d3d3e
Hannah Nesser <hnesser@g.harvard.edu>
2021-08-24 16:26:50 -0400

    Moving inversion functionality from class to package - Moved standard inversion functions from inversion.py (class) to invpy.py (package) - Removed obs_mod_diff function entirely (avoiding matrix multiplication) - Wrote a function to allow for either vector or matrix error covariances to be provided at minimal computaitonal cost - Moved reduced rank inversion functions from class to package (up until edecomp)

--- python/invpy.py
+++ python/invpy.py
@@ -3,11 +3,13 @@ Generic inversion functions (this is integrated with the inversion class).
 '''
 
 import numpy as np
+from numpy.linalg import inv
 import pandas as pd
 import xarray as xr
 import pickle
 import math
 from scipy.stats import linregress
+from scipy.linalg import sqrtm
 
 from os.path import join
 from os import listdir
@@ -25,6 +27,41 @@ import sys
 sys.path.append('.')
 import format_plots as fp
 import config
+
+## -------------------------------------------------------------------------##
+## Utility functions
+## -------------------------------------------------------------------------##
+def find_dimension(data):
+    if len(data.shape) == 1:
+        return 1
+    elif (data.shape[1] == 1) or (data.shape[0] == 1):
+        return 1
+    else:
+        return 2
+
+def inv_cov_matrix(data):
+    if find_dimension(data) == 1:
+        return np.diag(1/data)
+    else:
+        return inv(data)
+
+def sqrt_cov_matrix(data):
+    if find_dimension(data) == 1:
+        return np.diag(data**0.5)
+    else:
+        return sqrtm(data)
+
+def multiply_data_by_inv_cov(data, data_cov):
+    if find_dimension(data_cov) == 1:
+        return data/data_cov
+    else:
+        return data @ inv(data_cov)
+
+def multiply_data_by_sqrt_cov(data, data_cov):
+    if find_dimension(data_cov) == 1:
+        return data*data_cov**0.5
+    else:
+        return data @ sqrtm(data_cov)
 
 ## -------------------------------------------------------------------------##
 ## Cluster functions
@@ -161,6 +198,199 @@ def get_one_statevec_layer(data, category=None, time=None,
         data_out = data_out[cluster_idx]
 
     return data_out
+
+## -------------------------------------------------------------------------##
+## Standard inversion functions
+## -------------------------------------------------------------------------##
+def calculate_c(k, ya, xa):
+    '''
+    Calculate c for the forward model, defined as ybase = Kxa + c.
+    Save c as an element of the object.
+    '''
+    c = ya - k @ xa
+    return c
+
+def cost_func(ynew, y, so, xnew, xa, sa):
+    '''
+    Calculate the value of the Bayesian cost function
+        J(x) = (x - xa)T Sa (x-xa) + regularization_factor(y - Kx)T So (y - Kx)
+    for a given x. Prints out that value and the contributions from
+    the emission and observational terms.
+
+    Parameters:
+        x      The state vector at which to evaluate the cost function
+        y      The observations vector at which to evaluate the cost function
+               Note that by definition y=Kx+c, so: ya=Kxa+c, and yhat=Kxhat+c
+    Returns:
+        cost   The value of the cost function at x
+    '''
+
+    # Calculate the observational component of the cost function
+    cost_obs = (ynew - y).T @ inv_cov_matrix(so) @ (ynew - y)
+
+    # Calculate the emissions/prior component of the cost function
+    cost_emi = (xnew - xa).T @ inv_cov_matrix(sa) @ (xnew - xa)
+
+    # Calculate the total cost, print out information on the cost, and
+    # return the total cost function value
+    cost = cost_obs + cost_emi
+    print('     Cost function: %.2f (Emissions: %.2f, Observations: %.2f)'
+          % (cost, cost_emi, cost_obs))
+    return cost
+
+def solve_inversion(k, y, ya, so, xa, sa, calculate_cost=True):
+    '''
+    Calculate the solution to an analytic Bayesian inversion for the
+    given Inversion object. The solution includes the posterior state
+    vector (xhat), the posterior error covariance matrix (shat), and
+    the averaging kernel (A). The function prints out progress statements
+    and information about the posterior solution, including the value
+    of the cost function at the prior and posterior, the number of
+    negative state vector elements in the posterior solution, and the
+    DOFS of the posterior solution.
+    '''
+    print('... Solving inversion ...')
+
+    # Check if the errors are diagonal or not and, if so, modify
+    # calculations involving the error covariance matrices
+    sainv = inv_cov_matrix(sa)
+    kTsoinv = multiply_data_by_inv_cov(k.T, so)
+
+    # Calculate the cost function at the prior.
+    if calculate_cost:
+        print('Calculating the cost function at the prior mean.')
+        cost_prior = cost_func(ya, y, so, xa, xa, sa)
+
+    # Calculate the posterior error.
+    print('Calculating the posterior error.')
+    shat = np.array(inv(kTsoinv @ k + sainv))
+
+    # Calculate the posterior mean
+    print('Calculating the posterior mean.')
+    xhat = np.array(xa + (shat @ kTsoinv @ (y - ya)))
+    print('     Negative cells: %d' % xhat[xhat < 0].sum())
+
+    # Calculate the averaging kernel.
+    print('Calculating the averaging kernel.')
+    a = np.array(identity(xa.shape[0]) - shat @ sainv)
+    dofs = np.diag(a)
+    print('     DOFS: %.2f' % np.trace(a))
+
+    # Calculate the new set of modeled observations.
+    print('Calculating updated modeled observations.')
+    yhat = np.array(k @ xhat + c)
+
+    # Calculate the cost function at the posterior. Also calculate the
+    # number of negative cells as an indicator of inversion success.
+    if calculate_cost:
+        print('Calculating the cost function at the posterior mean.')
+        cost_post = cost_func(yhat, y, so, xhat, xa, sa)
+
+    print('... Complete ...\n')
+
+    return xhat, shat, a, yhat
+
+## -------------------------------------------------------------------------##
+## Reduced rank inversion functions
+## -------------------------------------------------------------------------##
+def get_rank(evals_q=None, evals_h=None, pct_of_info=None, rank=None, snr=None):
+    # Check whether evals_q or evals_h are provided:
+    if sum(x is not None for x in [evals_q, evals_h]) == 0:
+        raise AttributeError('Must provide one of evals_q or evals_h.')
+    elif evals_h is None:
+        evals_h = evals_q/(1 - evals_q)
+    elif  evals_q is None:
+        evals_q = evals_h/(1 + evals_h)
+
+    # Calculate the cumulative fraction of information content explained
+    # by each subsequent eigenvector
+    frac = np.cumsum(evals_q/evals_q.sum())
+
+    # Obtain the rank, requiring one and only one of pct_of_info, rank,
+    # or snr to be provided
+    if sum(x is not None for x in [pct_of_info, rank, snr]) > 1:
+        raise AttributeError('Provide only one of pct_of_info, rank, or snr.')
+    elif sum(x is not None for x in [pct_of_info, rank, snr]) == 0:
+        raise AttributeError('Must provide one of pct_of_info, rank, or snr.')
+    elif pct_of_info is not None:
+        diff = np.abs(frac - pct_of_info)
+        rank = np.argwhere(diff == np.min(diff))[0][0]
+        print('Calculated rank from percent of information: %d' % rank)
+        print('     Percent of information: %.4f%%' % (100*pct_of_info))
+        print('     Signal-to-noise ratio: %.2f'
+              % (evals_h[rank])**0.5)
+    elif snr is not None:
+        diff = np.abs(evals_h**0.5 - snr)
+        rank = np.argwhere(diff == np.min(diff))[0][0]
+        print('Calculated rank from signal-to-noise ratio : %d' % rank)
+        print('     Percent of information: %.4f%%' % (100*frac[rank]))
+        print('     Signal-to-noise ratio: %.2f' % snr)
+    elif rank is not None:
+        print('Using defined rank: %d' % rank)
+        print('     Percent of information: %.4f%%' % (100*frac[rank]))
+        print('     Signal-to-noise ratio: %.2f'
+              % (evals_h[rank])**0.5)
+    return rank
+
+def pph(k, so, sa, big_mem=False):
+    sasqrt_kt = multiply_data_by_sqrt_cov(k, sa)
+    sasqrt_kt_soinv = multiply_data_by_inv_cov(sasqrt_kt.T, so)
+
+    if big_mem:
+        pph = da.tensordot(sasqrt_kt_soinv, sasqrt_kt, axes=(1, 0))
+        pph = xr.DataArray(pph, dims=['nstate_0', 'nstate_1'],
+                           name=f'pph')
+    else:
+        pph = sasqrt_kt_soinv @ sasqrt_kt
+
+    print('Calculated PPH.')
+    return pph
+
+def edecomp(matrix, eval_threshold=None, number_of_evals=None):
+    print('... Calculating eigendecomposition ...')
+
+    # Check that the matrix is symmetric
+    assert np.allclose(matrix, matrix.T, rtol=1e-5), \
+           'The provided matrix is not symmetric.'
+
+    # Perform the eigendecomposition of the prior pre-conditioned Hessian
+    # We return the evals of the projection, not of the
+    # prior pre-conditioned Hessian.
+    if (eval_threshold is None) and (number_of_evals is None):
+         evals, evecs = eigh(pph)
+    elif (eval_threshold is None):
+        n = pph.shape[0]
+        evals, evecs = eigh(pph, subset_by_index=[n - number_of_evals,
+                                                  n - 1])
+    else:
+        evals, evecs = eigh(pph, subset_by_value=[eval_threshold, np.inf])
+    print('Eigendecomposition complete.')
+
+    # Sort evals and evecs by eval
+    idx = np.argsort(evals)[::-1]
+    evals = evals[idx]
+    evecs = evecs[:,idx]
+
+    # Force all evals to be non-negative
+    if (evals < 0).sum() > 0:
+        print('Negative eigenvalues. Maximum negative value is %.2e. Setting negative eigenvalues to zero.' \
+            % (evals[evals < 0].min()))
+        evals[evals < 0] = 0
+
+    # Check for imaginary eigenvector components and force all
+    # eigenvectors to be only the real component.
+    if np.any(np.iscomplex(evecs)):
+        print('Imaginary eigenvectors exist at index %d of %d. Forcing eigenvectors to real component alone.' \
+              % ((np.where(np.iscomplex(evecs))[1][0] - 1), len(evecs)))
+        evecs = np.real(evecs)
+
+    # Saving result to our instance.
+    print('Saving eigenvalues and eigenvectors to instance.')
+    # self.evals = evals/(1 + evals)
+    self.evals_h = evals
+    self.evals_q = evals/(1 + evals)
+    self.evecs = evecs
+    print('... Complete ...\n')
 
 ## -------------------------------------------------------------------------##
 ## Plotting functions : state vectors
