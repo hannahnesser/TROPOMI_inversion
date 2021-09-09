cdab0d17d2b168f0024c070d2cc69017753f7078
Hannah Nesser <hnesser@g.harvard.edu>
2021-09-09 12:45:10 -0400

    This script is still in a lot of flux.

--- python/analyze_k0.py
+++ python/analyze_k0.py
@@ -19,7 +19,7 @@ import gcpy as gc
 import troppy as tp
 import invpy as ip
 import format_plots as fp
-import inversion_settings as settings
+import inversion_settings as s
 
 ## ------------------------------------------------------------------------ ##
 ## Set user preferences
@@ -31,98 +31,212 @@ data_dir = base_dir + 'inversion_data/'
 plot_dir = base_dir + 'plots/'
 
 # Colormaps
-c = plt.cm.get_cmap('inferno', lut=10)
+# c = plt.cm.get_cmap('inferno', lut=10)
 plasma_trans = fp.cmap_trans('plasma')
 plasma_trans_r = fp.cmap_trans('plasma_r')
 rdbu_trans = fp.cmap_trans_center('RdBu_r', nalpha=70)
+r_trans = fp.cmap_trans('Reds', nalpha=100)
 viridis_trans_r = fp.cmap_trans('viridis_r')
 magma_trans = fp.cmap_trans('magma')
 # print(viridis_trans)
 
 # Small (i.e. non-default) figure settings
-small_fig_kwargs = {'max_width' : 3.25,
-                    'max_height' : 3}
 small_map_kwargs = {'draw_labels' : False}
+small_fig_kwargs = {'max_width' : 4,
+                    'max_height' : 3.5}
+
+# Load clusters
+clusters = xr.open_dataarray(f'{data_dir}clusters.nc')
 
 ########################################################################
-### FIGURE 2: AVERAGING KERNEL SENSITIVITY TO PRIOR AND OBSERVATIONS ###
+### FIGURE : EIGENVALUES
 ########################################################################
+evals_q = np.load(f'{data_dir}evals_q0.npy')
+evals_h = np.load(f'{data_dir}evals_h0.npy')
+# evals_h[evals_h < 0] = 0
+snr = evals_h**0.5
+DOFS_frac = np.cumsum(evals_q)/evals_q.sum()
 
-# Load initial averaging kernel
-dofs = np.loadtxt(f'{data_dir}dofs0_evec_method.csv', delimiter=',')
-# dofs = dofs**0.
-# dofs += 0.4
+fig, ax = fp.get_figax(aspect=3)
+
+# DOFS frac
+ax.plot(DOFS_frac, label='Information content spectrum',
+        c=fp.color(3), lw=1)
+for p in [0.5, 0.9, 0.95, 0.97, 0.98, 0.99]:
+    diff = np.abs(DOFS_frac - p)
+    rank = np.argwhere(diff == np.min(diff))[0][0]
+    print(p, rank)
+    ax.scatter(rank, DOFS_frac[rank], marker='*', s=20, c=fp.color(3))
+    ax.text(rank, DOFS_frac[rank]-0.05, f'{int(100*p):d}%%',
+            ha='center', va='top', c=fp.color(3))
+ax.set_xlabel('Eigenvector index', fontsize=config.LABEL_FONTSIZE*config.SCALE,
+              labelpad=config.LABEL_PAD)
+ax.set_ylabel('Fraction of DOFS', fontsize=config.LABEL_FONTSIZE*config.SCALE,
+              labelpad=config.LABEL_PAD, color=fp.color(3))
+ax.tick_params(axis='both', which='both',
+               labelsize=config.LABEL_FONTSIZE*config.SCALE)
+ax.tick_params(axis='y', labelcolor=fp.color(3))
+
+# SNR
+ax2 = ax.twinx()
+ax2.plot(snr, label='Signal-to-noise ratio spectrum', c=fp.color(6), lw=1)
+for r in [1, 2]:
+    diff = np.abs(snr - r)
+    rank = np.argwhere(diff == np.min(diff))[0][0]
+    ax2.scatter(rank, snr[rank], marker='.', s=20, c=fp.color(6))
+    ax2.text(rank-200, snr[rank], f'{r:d}', ha='right', va='center',
+             c=fp.color(6))
+ax2.set_ylabel('Signal-to-noise ratio',
+               fontsize=config.LABEL_FONTSIZE*config.SCALE,
+              labelpad=config.LABEL_PAD, color=fp.color(6))
+ax2.tick_params(axis='y', which='both',
+                labelsize=config.LABEL_FONTSIZE*config.SCALE,
+                labelcolor=fp.color(6))
+
+ax = fp.add_title(ax, 'Initial Estimate Information Content Spectrum')
+
+fp.save_fig(fig, plot_dir, 'eigenvalues_update')
+
+########################################################################
+### FIGURE : JACOBIAN COLUMN
+########################################################################
+# # Load observations to get lat/lon
+# obs = gc.read_file(f'{data_dir}2019_corrected.pkl')
+# obs = obs[obs['MONTH'] == 6]
+
+# # Load Jacobian column
+# ki = gc.read_file(f'{data_dir}k0_m06_sample_column.nc').values
+# # print(ki.max())
+
+# # Create plot
+# fig, ax = fp.get_figax(maps=True, lats=clusters.lat, lons=clusters.lon)
+# ax = fp.format_map(ax, clusters.lat, clusters.lon, **small_map_kwargs)
+
+# # Plot
+# c = ax.scatter(obs['LON'], obs['LAT'], c=ki,
+#                cmap=r_trans, vmin=0, vmax=0.03,
+#                s=0.25)
+
+# # Add colorbar
+# cax = fp.add_cax(fig, ax)
+# cb = fig.colorbar(c, ax=ax, cax=cax)
+# cb = fp.format_cbar(cb, 'Jacobian Value')
+
+# # Save
+# fp.save_fig(fig, plot_dir, f'fig_k0')
+
+# ########################################################################
+# ### FIGURE : INITIAL AVERAGING KERNEL SENSITIVITIES
+# ########################################################################
+# Load clusters
 clusters_plot = xr.open_dataarray(f'{data_dir}clusters.nc')
-# dofs = np.diag(a)
 
-# Initial estimate averaging kernel
-# title = 'Initial estimate averaging\nkernel sensitivities'
-title = 'Initial estimate KT SO-1 K'
-# avker_cbar_kwargs = {'title' : r'$\partial\hat{x}_i/\partial x_i$'}
-avker_cbar_kwargs = {'title' : ''}
+# Standard plotting preferences
+avker_cbar_kwargs = {'title' : r'$\partial\hat{x}_i/\partial x_i$'}
 avker_kwargs = {'cmap' : plasma_trans, 'vmin' : 0, 'vmax' : 1,
                 'cbar_kwargs' : avker_cbar_kwargs,
-                'fig_kwargs' : small_fig_kwargs,
                 'map_kwargs' : small_map_kwargs}
-fig, ax, c = ip.plot_state(dofs, clusters_plot, title=title,
-                             **avker_kwargs)
-# ax.text(0.025, 0.05, 'DOFS = %d' % round(dofs.sum()),
-#         fontsize=config.LABEL_FONTSIZE*config.SCALE,
-#         transform=ax.transAxes)
-fp.save_fig(fig, plot_dir, 'fig_est0_dofs_evec_method')
 
-# Load evecs
-# evecs = np.loadtxt(f'{data_dir}evecs.csv', delimiter=',')
-# evecs0 = evecs[:, 0]
-# cbar_kwargs = {'title' : 'Eigenvector Value'}
-# fig, ax, c = ip.plot_state(evecs0, clusters_plot, title='Eigenvector 1',
-#                            cmap=rdbu_trans, cbar_kwargs=cbar_kwargs,
-#                            vmin=-0.1, vmax=0.1,
+
+# Fraction of information contents
+# fracs = [90, 95, 98, 99, 99.9]
+# fracs = [99.9]
+suffixes = ['poi50', 'poi90', 'poi95', 'poi99', 'poi99.9']
+
+# Load initial averaging kernel
+for f in suffixes:
+    dofs = np.load(f'{data_dir}dofs0_{f}.npy')
+
+    title = f'Initial estimate averaging kernel sensitivities' # ({f}\%)'
+    fig, ax, c = ip.plot_state(dofs, clusters_plot, title=title,
+                               **avker_kwargs)
+    ax.text(0.025, 0.05, 'DOFS = %d' % round(dofs.sum()),
+            fontsize=config.LABEL_FONTSIZE*config.SCALE,
+            transform=ax.transAxes)
+    fp.save_fig(fig, plot_dir, f'fig_est0_dofs_{f}_update')
+
+# ########################################################################
+# ### FIGURE : PRIOR ERROR STANDARD DEVIATION
+# ########################################################################
+# # Load prior error
+# sa_vec = gc.read_file(f'{data_dir}sa_abs.nc')
+# sd_vec = sa_vec**0.5
+# cbar_kwargs = {'title' : 'Mg km$^2$ a$^{-1}$'}
+# fig, ax, c = ip.plot_state(sd_vec, clusters,
+#                            title='Prior error\nstandard deviation',
+#                            cmap=fp.cmap_trans('viridis'), vmin=0, vmax=10,
 #                            fig_kwargs=small_fig_kwargs,
-#                            map_kwargs=small_fig_kwargs)
-# fp.save_fig(fig, plot_dir, 'fig_est0_evec0')
+#                            cbar_kwargs=cbar_kwargs,
+#                            map_kwargs=small_map_kwargs)
+# fp.save_fig(fig, plot_dir, 'fig_prior_error')
 
-# # Prior error
-# true.sd_vec = true.sa_vec**0.5
-# true.sd_vec_abs = true.sd_vec*true.xa_abs
-# cbar_kwargs = {'title' : 'Tg/month'}
-# fig2c, ax, c = true.plot_state('sd_vec_abs', clusters_plot,
-#                                 title='Prior error standard deviation',
-#                                 cmap=fp.cmap_trans('viridis'),
-#                                 vmin=0, vmax=15,
-#                                 fig_kwargs=small_fig_kwargs,
-#                                 cbar_kwargs=cbar_kwargs,
-#                                 map_kwargs=small_map_kwargs)
-# fp.save_fig(fig2c, plots, 'fig2c_prior_error')
+# ########################################################################
+# ### FIGURE : OBSERVATIONAL DENSITY
+# ########################################################################
+# # Load observations
+# obs = gc.read_file(f'{data_dir}2019_corrected.pkl')
 
-# # Observational density
-# lat_res = np.diff(clusters_plot.lat)[0]
-# lat_edges = np.append(clusters_plot.lat - lat_res/2,
-#                       clusters_plot.lat[-1] + lat_res/2)
-# # lat_edges = lat_edges[::2]
-# obs['lat_edges'] = pd.cut(obs['lat'], lat_edges, precision=4)
+# # Get latitude and longitude edges
+# lat_e = np.arange(s.lat_min, s.lat_max + s.lat_delta, s.lat_delta)
+# lon_e = np.arange(s.lon_min, s.lon_max + s.lon_delta, s.lon_delta)
 
-# lon_res = np.diff(clusters_plot.lon)[0]
-# lon_edges = np.append(clusters_plot.lon - lon_res/2,
-#                       clusters_plot.lon[-1] + lon_res/2)
-# # lon_edges = lon_edges[::2]
-# obs['lon_edges'] = pd.cut(obs['lon'], lon_edges, precision=4)
+# # Put each observation into a grid box
+# obs['LAT_E'] = pd.cut(obs['LAT'], lat_e, precision=4)
+# obs['LON_E'] = pd.cut(obs['LON'], lon_e, precision=4)
 
-# obs_density = obs.groupby(['lat_edges', 'lon_edges']).count()
-# obs_density = obs_density['Nobs'].reset_index()
-# obs_density['lat'] = obs_density['lat_edges'].apply(lambda x: x.mid)
-# obs_density['lon'] = obs_density['lon_edges'].apply(lambda x: x.mid)
-# obs_density = obs_density.set_index(['lat', 'lon'])['Nobs']
+# # Group by grid box (could probably do this by iGC/jGC but oh well)
+# obs_density = obs.groupby(['LAT_E', 'LON_E']).count()
+# obs_density = obs_density['OBS'].reset_index()
+
+# # Get the edges of each grid box and set these as the indices
+# obs_density['lat'] = obs_density['LAT_E'].apply(lambda x: x.mid)
+# obs_density['lon'] = obs_density['LON_E'].apply(lambda x: x.mid)
+# obs_density = obs_density.set_index(['lat', 'lon'])['OBS']
+
+# # Convert to xarray
 # obs_density = obs_density.to_xarray()
 
-# title = 'GOSAT observation density\n(July 2009)'
-# viridis_trans_long = fp.cmap_trans('viridis', nalpha=90, ncolors=300)
-# cbar_kwargs = {'ticks' : np.arange(0, 25, 5),
+# # Plot
+# title = 'Observation density'
+# viridis_trans_long = fp.cmap_trans('viridis', nalpha=25, ncolors=300)
+# cbar_kwargs = {'ticks' : np.arange(0, 1100, 200),
 #                'title' : 'Count'}
-# fig2d, ax, c = true.plot_state_format(obs_density, title=title,
-#                                        vmin=0, vmax=10, default_value=0,
-#                                        cmap=viridis_trans_long,
-#                                        fig_kwargs=small_fig_kwargs,
-#                                        cbar_kwargs=cbar_kwargs,
-#                                        map_kwargs=small_map_kwargs)
-# fp.save_fig(fig2d, plots, 'fig2d_gosat_obs_density')
+# fig, ax, c = ip.plot_state_format(obs_density, title=title,
+#                                   vmin=0, vmax=1000, default_value=0,
+#                                   cmap=viridis_trans_long,
+#                                   fig_kwargs=small_fig_kwargs,
+#                                   cbar_kwargs=cbar_kwargs,
+#                                   map_kwargs=small_map_kwargs)
+# fp.save_fig(fig, plot_dir, 'fig_tropomi_obs_density')
+
+########################################################################
+### FIGURE : INITIAL EIGENVECTORS
+########################################################################
+# # Eigenvectors
+# # evecs = [f'{data_dir}evec_pert_{i:02d}.nc' for i in range(1, 10)]
+# # i = 1
+# # for v in evecs:
+# #     ev = xr.open_dataarray(v)
+# #     fig, ax, c = ip.plot_state_format(ev, cmap='RdBu_r',
+# #                                       vmin=-0.1, vmax=0.1)
+# #     fp.save_fig(fig, plot_dir, f'fig_est0_evec_{i:02d}')
+# #     i += 1
+
+# evecs = 'evecs0.npy'
+# evecs = 0.5*np.load(f'{data_dir}{evecs}')
+# clusters = xr.open_dataarray(f'{data_dir}clusters.nc')
+# fig, ax, c = ip.plot_state_grid(evecs[:,:2], 1, 2, clusters,
+#                                 titles=[f'Eigenvector {i}' for i in range(1, 3)],
+#                                 cmap='RdBu_r', vmin=-0.01, vmax=0.01,
+#                                 cbar_kwargs={'title' : 'Eigenvector\nvalue',
+#                                              'ticks' : [-0.01, 0, 0.01]})
+# fp.save_fig(fig, plot_dir, 'fig_est0_evecs')
+
+# evals = 'evals_h0.npy'
+# evals = np.load(f'{data_dir}{evals}')
+# evals[evals < 0] = 0
+# diff = np.abs(evals**0.5 - 1.25)
+# rank = np.argwhere(diff == np.min(diff))[0][0]
+# print(rank)
+
+
