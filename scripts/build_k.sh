e7149247bda212cbe8c3addc3b585712df2e222a
Hannah Nesser <hnesser@g.harvard.edu>
2021-10-13 11:34:32 -0400

    Yet more attempts

--- scripts/build_k.sh
+++ scripts/build_k.sh
@@ -5,4 +5,6 @@ PERT_DIRS='/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TR
 DATA_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/initial_inversion"
 CODE_DIR="/n/home04/hnesser/TROPOMI_inversion/python"
 
+echo ${PERT_DIRS}
+
 sbatch --array=1 build_k_monthly.sh ${PRIOR_DIR} ${PERT_DIRS} ${DATA_DIR} ${CODE_DIR}
