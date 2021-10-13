22d8f45e99e7cbe89b04c2324ddaaf9e6b9a7b48
Hannah Nesser <hnesser@g.harvard.edu>
2021-10-13 10:50:22 -0400

    Test 2

--- scripts/build_k.sh
+++ scripts/build_k.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PRIOR_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TROPOMI_inversion_0000_final"
-PERT_DIRS="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TROPOMI_inversion_\?\?\?\?"
+PERT_DIRS=""/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TROPOMI_inversion_????""
 DATA_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/initial_inversion"
 CODE_DIR="/n/home04/hnesser/TROPOMI_inversion/python"
 
