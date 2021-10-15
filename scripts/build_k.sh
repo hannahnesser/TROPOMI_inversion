e04776c607a937c8df6e2bae25ec1240b3969be0
Hannah Nesser <hnesser@g.harvard.edu>
2021-10-15 17:05:14 -0400

    Expanding to remaining months

--- scripts/build_k.sh
+++ scripts/build_k.sh
@@ -5,4 +5,4 @@ PERT_DIRS="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TR
 DATA_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/initial_inversion"
 CODE_DIR="/n/home04/hnesser/TROPOMI_inversion/python"
 
-sbatch --array=2-4 build_k_monthly.sh ${PRIOR_DIR} ${PERT_DIRS} ${DATA_DIR} ${CODE_DIR}
+sbatch --array=5-12 build_k_monthly.sh ${PRIOR_DIR} ${PERT_DIRS} ${DATA_DIR} ${CODE_DIR}
