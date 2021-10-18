c5b01d44f050a9532882c09e47d573db0b984400
Hannah Nesser <hnesser@g.harvard.edu>
2021-10-18 16:00:57 -0400

    Added a comment

--- scripts/iterate_1.sh
+++ scripts/iterate_1.sh
@@ -3,7 +3,7 @@
 PRIOR_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TROPOMI_inversion_0000_final"
 PERT_DIRS="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs/TROPOMI_inversion_????"
 SHORT_TERM_DATA_DIR="/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/initial_inversion"
-LONG_TERM_DATA_DIR="/n/seasasfs02/hnesser/TROPOMI_inversion/"
+LONG_TERM_DATA_DIR="/n/seasasfs02/hnesser/TROPOMI_inversion"
 CODE_DIR="/n/home04/hnesser/TROPOMI_inversion/python"
 
 # Build the Jacobian
@@ -11,7 +11,7 @@ jid1=$(sbatch --array=1-12 build_k_monthly.sh ${PRIOR_DIR} ${PERT_DIRS} ${SHORT_
 
 # Calculate the prior preconditioned Hessian
 jid2=$(sbatch --dependency=afterok:${jid1##* } --array=1-12 generate_pph.sh "1" ${SHORT_TERM_DATA_DIR} ${CODE_DIR})
-# sbatch --array=1-12 generate_pph.sh ${SHORT_TERM_DATA_DIR} ${CODE_DIR}
+# sbatch --array=1 generate_pph.sh "1" ${SHORT_TERM_DATA_DIR} ${CODE_DIR}
 
 # Calculate the eigenvectors
 jid3=$(sbatch --dependency=afterok:${jid2##* } generate_evecs.sh "1" ${SHORT_TERM_DATA_DIR} ${LONG_TERM_DATA_DIR} ${CODE_DIR})
