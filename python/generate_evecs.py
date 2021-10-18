de0dc33e9ef8c532dff13fc46b5076caed5c944b
Hannah Nesser <hnesser@g.harvard.edu>
2021-10-18 15:41:13 -0400

    Switch to generic (iteration independent) script

--- python/generate_evecs.py
+++ python/generate_evecs.py
@@ -1,150 +1,221 @@
-# import copy
-import os
-from os.path import join
-import math
-# import itertools
-import pickle
+if __name__ == '__main__':
+    import sys
+    from datetime import datetime
+    import xarray as xr
+    import numpy as np
+    import pandas as pd
+    from scipy.linalg import eigh
+    import matplotlib.pyplot as plt
 
-import sys
-sys.path.append('/n/home04/hnesser/TROPOMI_inversion/python')
-import inversion as inv
+    ## ---------------------------------------------------------------------##
+    ## Set user preferences
+    ## ---------------------------------------------------------------------##
+    local = False
 
-import xarray as xr
-import numpy as np
-import pandas as pd
-from scipy.linalg import eigh
+    # Cannon
+    if not local:
+        # code_dir = '/n/home04/hnesser/TROPOMI_inversion/python/'
+        # data_dir = '/n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/initial_inversion/'
+        # output_dir = '/n/seasasfs02/hnesser/TROPOMI_inversion/inversion_data/'
+        niter = sys.argv[1]
+        data_dir = sys.argv[2]
+        output_dir = sys.argv[3]
+        code_dir = sys.argv[4]
+    else:
+        base_dir = '/Users/hannahnesser/Documents/Harvard/Research/TROPOMI_Inversion/'
+        code_dir = f'{base_dir}python/'
+        data_dir = f'{base_dir}inversion_data/'
 
-## -------------------------------------------------------------------------##
-## Set user preferences
-## -------------------------------------------------------------------------##
-# Cannon
-base_dir = '/n/seasasfs02/hnesser/TROPOMI_inversion/'
-code_dir = '/n/home04/hnesser/TROPOMI_inversion/python/'
-data_dir = f'{base_dir}inversion_data/'
+    # User preferences
+    calculate_evecs = False
+    format_evecs = True
+    n_evecs = int(10)
+    calculate_avker = False
+    pct_of_info = [50, 90, 95, 99, 99.9]
+    snr = None
+    rank = None
 
-# The prior_run can either be a list of files or a single file
-# with all of the data for simulation
-year = 2019
-months = np.arange(1, 12, 1) # excluding December for now
-days = np.arange(1, 32, 1)
+    ## -------------------------------------------------------------------- ##
+    ## Set up working environment
+    ## -------------------------------------------------------------------- ##
+    # Import custom packages
+    import sys
+    sys.path.append(code_dir)
+    import inversion as inv
+    import inversion_settings as s
+    import gcpy as gc
+    import invpy as ip
+    import format_plots as fp
+    import config as c
 
-# Files
-emis_file = f'{base_dir}prior/total_emissions/HEMCO_diagnostics.{year}.nc'
-obs_file = f'{base_dir}observations/{year}_corrected.pkl'
-cluster_file = f'{data_dir}clusters_0.25x0.3125.nc'
-k_nstate = f'{data_dir}k0_nstate.pkl'#None
+    if not local:
+        # Import dask things
+        from dask.distributed import Client, LocalCluster, progress
+        from dask.diagnostics import ProgressBar
+        import dask.config
+        dask.config.set({'distributed.comm.timeouts.connect' : 90,
+                         'distributed.comm.timeouts.tcp' : 150,
+                         'distributed.adaptive.wait-count' : 90,
+                         'temporary_directory' : data_dir})
 
-# Which analyses do you wish to perform?
+        # Open cluster and client
+        n_workers = 2
+        threads_per_worker = 2
+        cluster = LocalCluster(n_workers=n_workers,
+                               threads_per_worker=threads_per_worker)
+        client = Client(cluster)
 
-# Information on the grid
-lat_bins = np.arange(10, 65, 5)
-lat_min = 9.75
-lat_max = 60
-lat_delta = 0.25
-lon_min = -130
-lon_max = -60
-lon_delta = 0.3125
-buffers = [3, 3, 3, 3]
+        # We now calculate chunk size.
+        n_threads = n_workers*threads_per_worker
+        nstate_chunk = 1e3
+        chunks = {'nstate_0' : nstate_chunk, 'nstate_1' : nstate_chunk}
+        print('State vector chunks : ', nstate_chunk)
 
-## -------------------------------------------------------------------------##
-## Load data
-## -------------------------------------------------------------------------##
+    ## -------------------------------------------------------------------- ##
+    ## Load global quantities
+    ## -------------------------------------------------------------------- ##
+    # State vector dimension
+    sa = gc.read_file(f'{data_dir}sa.nc')
+    sa = sa.values.reshape(-1, 1)
+    nstate = sa.shape[0]
 
-data_dir='/n/seasasfs02/hnesser/TROPOMI_inversion/evec_perturbations_ZQ'
+    ## ---------------------------------------------------------------------##
+    ## Calculate the eigenvectors
+    ## ---------------------------------------------------------------------##
+    if calculate_evecs:
+        # Sum together the monthly PPHs
+        pph = xr.DataArray(np.zeros((nstate, nstate)),
+                           dims=['nstate_0', 'nstate_1'], name=f'pph{niter}')
+        for m in s.months:
+            print(f'Loading month {m}.')
+            temp = xr.open_dataarray(f'{data_dir}pph{niter}_m{m:02d}.nc')
+            pph += temp
 
-# Jacobian
-k = load_obj(join(data_dir, 'k.pkl')).astype('float32')
+        # Load pph into memory
+        pph = pph.compute()
 
-# Native-resolution prior and prior error
-xa = load_obj(join(data_dir, 'xa.pkl')).reshape(-1, 1).astype('float32')
-sa_vec = load_obj(join(data_dir, 'sa_vec.pkl')).reshape(-1, 1).astype('float32')
+        # Calculate the eigenvectors (this is the time consuming step)
+        evals_h, evecs = eigh(pph)
 
-# Vectorized observations and error
-y = load_obj(join(data_dir, 'y.pkl')).reshape(-1, 1).astype('float32')
-y_base = load_obj(join(data_dir, 'y_base.pkl')).reshape(-1, 1).astype('float32')
-so_vec = load_obj(join(data_dir, 'so_vec.pkl')).reshape(-1, 1).astype('float32')
+        # Sort them
+        idx = np.argsort(evals_h)[::-1]
+        evals_h = evals_h[idx]
+        evecs = evecs[:, idx]
 
-## -------------------------------------------------------------------------##
-## Set constants
-## -------------------------------------------------------------------------##
+        # Force all evals to be non-negative
+        if (evals_h < 0).sum() > 0:
+            print('Negative eigenvalues. Maximum negative value is %.2e. Setting negative eigenvalues to zero.' \
+                % (evals_h[evals_h < 0].min()))
+            evals_h[evals_h < 0] = 0
 
-RF = 0.001
+        # Check for imaginary eigenvector components and force all
+        # eigenvectors to be only the real component.
+        if np.any(np.iscomplex(evecs)):
+            print('Imaginary eigenvectors exist at index %d of %d. Forcing eigenvectors to real component alone.' \
+                  % ((np.where(np.iscomplex(evecs))[1][0] - 1), len(evecs)))
+            evecs = np.real(evecs)
 
-## -------------------------------------------------------------------------##
-## Create inversion object
-## -------------------------------------------------------------------------##
+        # Calculate evals_q
+        evals_q = evals_h/(1 + evals_h)
 
-# Create a true Reduced Rank Jacobian object
-inv_zq = inv.ReducedRankJacobian(k, xa, sa_vec, y, y_base, so_vec)
-inv_zq.rf = RF
+        # Calculate the prolongation and reduction operators
+        prolongation = (sa**0.5) * evecs
+        reduction = evecs.T * (1/sa**0.5)
 
-# Delete the other items from memory
-del(k)
-del(xa)
-del(sa_vec)
-del(y)
-del(y_base)
-del(so_vec)
+        # Save out the matrices
+        np.save(f'{data_dir}evecs{niter}.npy', evecs)
+        np.save(f'{data_dir}evals_h{niter}.npy', evals_h)
+        np.save(f'{data_dir}evals_q{niter}.npy', evals_q)
+        np.save(f'{data_dir}prolongation{niter}.npy', prolongation)
+        np.save(f'{data_dir}reduction{niter}.npy', reduction)
 
-# Save out the PPH
-pph = (inv_zq.sa_vec**0.5)*inv_zq.k.T
-pph = np.dot(pph, ((inv_zq.rf/inv_zq.so_vec)*pph.T))
-save_obj(pph, join(data_dir, 'pph.pkl'))
+        print('Eigendecomposition complete.\n')
 
-# Complete an eigendecomposition of the prior pre-
-# conditioned Hessian
-assert np.allclose(pph, pph.T, rtol=1e-5), \
-       'The prior pre-conditioned Hessian is not symmetric.'
+    else:
+        evals_h = np.load(f'{data_dir}evals_h{niter}.npy')
+        evecs = np.load(f'{data_dir}evecs{niter}.npy')
+        prolongation = np.load(f'{data_dir}prolongation{niter}.npy')
+        if not local:
+            evals_q = np.load(f'{data_dir}evals_q{niter}.npy')
+            reduction = np.load(f'{data_dir}reduction{niter}.npy')
+        else:
+            evals_q = evals_h/(1 + evals_h)
 
-evals, evecs = eigh(pph)
-print('Eigendecomposition complete.')
+    ## ---------------------------------------------------------------------##
+    ## Format the eigenvectors for HEMCO
+    ## ---------------------------------------------------------------------##
+    if format_evecs:
+        # First, print information about the rank/percent of information content/
+        # SNR associated with the number of evecs provided
+        DOFS_frac = np.cumsum(evals_q)/evals_q.sum()
+        print(f'SAVING OUT {n_evecs} EIGENVECTORS')
+        print(f'% DOFS : {(100*DOFS_frac[n_evecs])}')
+        print(f'SNR    : {(evals_h[n_evecs]**0.5)}')
 
-# Sort evals and evecs by eval
-idx = np.argsort(evals)[::-1]
-evals = evals[idx]
-evecs = evecs[:,idx]
+        # Load clusters
+        clusters = xr.open_dataarray(f'{data_dir}clusters.nc')
 
-# Force all evals to be non-negative
-if (evals < 0).sum() > 0:
-    print('Negative eigenvalues. Maximum negative value is %.2e. Setting negative eigenvalues to zero.' \
-        % (evals[evals < 0].min()))
-    evals[evals < 0] = 0
+        # Iterate through columns and save out HEMCO-appropriate files
+        for i in range(n_evecs):
+            pert = ip.match_data_to_clusters(prolongation[:, i], clusters, 0)
+            # pert = ip.match_data_to_clusters(prolongation[:, i], clusters, 0)
 
-# Check for imaginary eigenvector components and force all
-# eigenvectors to be only the real component.
-if np.any(np.iscomplex(evecs)):
-    print('Imaginary eigenvectors exist at index %d of %d. Forcing eigenvectors to real component alone.' \
-          % ((np.where(np.iscomplex(evecs))[1][0] - 1), len(evecs)))
-    evecs = np.real(evecs)
+            # Define HEMCO attributes
+            long_name = 'Eigenvector perturbations'
+            title_str = f'Eigenvector perturbation {i+1} for the construction of the Jacobian matrix for methane inversions.'
+            pert = gc.define_HEMCO_std_attributes(pert, name='evec_pert')
+            pert = gc.define_HEMCO_var_attributes(pert, 'evec_pert',
+                                                  long_name=long_name,
+                                                  units='kg/m2/s')
+            pert.attrs = {'Title' : title_str,
+                          'Conventions' : 'COARDS',
+                          'History' : datetime.now().strftime('%Y-%m-%d %H:%M')}
 
-# Saving result to our instance.
-print('Saving eigenvalues and eigenvectors.')
-# self.evals = evals/(1 + evals)
-np.savetxt(join(data_dir, 'evecs.csv'), evecs, delimiter=',')
-np.savetxt(join(data_dir, 'evals_h.csv'), evals, delimiter=',')
-np.savetxt(join(data_dir, 'evals_q.csv'), evals/(1+evals), delimiter=',')
-print('... Complete ...\n')
+            # Scaling (for a later date)
+            # for s in evec_scaling:
+            #     suffix = f'_{(i+1):04d}_{s}'
+            #     pert.attrs = {'Title' : title_str, 'Scaling' : s}
+            #     p = deepcopy(pert)
+            #     p['evec_pert'] *= float(s)
 
+            gc.save_HEMCO_netcdf(pert, f'{output_dir}inversion_data/eigenvectors{niter}', f'evec_pert_{(i+1):04d}.nc')
+            print(f'Saved eigenvector {(i+1)} : {output_dir}inversion_data/eigenvectors{niter}/evec_pert_{(i+1):04d}.nc')
 
-#
-#         # temp = ev.where(ev.perturbation == p, drop=True).drop('perturbation').squeeze('perturbation')
-d.lat.attrs = {'long_name': 'latitude', 'units': 'degrees_north'}
-d.lon.attrs = {'long_name': 'longitude', 'units': 'degrees_east'}
-d.time.attrs = {'long_name' : 'Time'}
-d['evec_perturbations'].attrs = {'long_name' : 'Eigenvector perturbations',
-                                 'units' : 'kg/m2/s'}
-d.to_netcdf('/n/seasasfs02/hnesser/TROPOMI_inversion/evec_perturbations_ZQ/evec_perturbations_0001_scaled_1e-8.nc',
-               encoding={'evec_perturbations' : {'_FillValue' : None,
-                                                 'dtype' : 'float32'},
-                         'time' : {'_FillValue' : None, 'dtype' : 'float32',
-                                   'calendar' : 'standard',
-                                   'units' : 'hours since 2009-01-01 00:00:00'},
-                         'lat' : {'_FillValue' : None, 'dtype' : 'float32'},
-                         'lon' : {'_FillValue' : None, 'dtype' : 'float32'}})
+    ## ---------------------------------------------------------------------##
+    ## Calculate the averaging kernel
+    ## ---------------------------------------------------------------------##
+    if calculate_avker:
+        print('Calculating averaging kernel.')
+        for p in pct_of_info:
+            print(f'Using {p} percent of information content.')
+            # Figure out the fraction of information content
+            # if sum(x is not None for x in [p, snr, rank]) > 1:
+            #     raise AttributeError('Conflicting rank arguments provided.')
+            # elif sum(x is not None for x in [p, snr, rank]) == 0:
+            #     raise AttributeError('Insufficient rank arguments provided.')
+            # elif p is not None:
+            rank = ip.get_rank(evals_q=evals_q, pct_of_info=p/100)
+            # diff = np.abs(DOFS_frac - (p/100))
+            # rank = np.argwhere(diff == np.min(diff))[0][0]
+            suffix = f'_poi{p}'
+            # elif snr is not None:
+            #     evals_h[evals_h < 0] = 0
+            #     diff = np.abs(evals_h**0.5 - snr)
+            #     rank = np.argwhere(diff == np.min(diff))[0][0]
+            #     suffix = f'_snr{snr}'
+            # else:
+            #     suffix = f'_rank{rank}'
+            # print(f'Rank = {rank}')
 
-# # need to set attribute dictionaries for lat
-# # lon, time, and the evec_pertuurbations, complete
-# # with units
+            # Subset the evals and evecs
+            evals_q_sub = evals_q[:rank]
+            evecs_sub = evecs[:, :rank]
 
-d.time.attrs = {'long_name' : 'Time', 'units' : 'hours since 2009-01-01 00:00:00', 'calendar' : 'standard'}
+            # Calculate the averaging kernel (we can leave off Sa when it's
+            # constant)
+            a = (evecs_sub*evals_q_sub) @ evecs_sub.T
 
+            # Save the result
+            np.save(f'{data_dir}a{niter}{suffix}.npy', a)
+            np.save(f'{data_dir}dofs{niter}{suffix}.npy', np.diagonal(a))
+    print('CODE COMPLETE')
