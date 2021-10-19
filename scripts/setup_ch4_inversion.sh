b6f5e7952a0256e58282a3eb01d435254b4987a8
Hannah Nesser <hnesser@g.harvard.edu>
2021-10-19 12:55:42 -0400

    ...Changing perturbation series again for a bug fix

--- scripts/setup_ch4_inversion.sh
+++ scripts/setup_ch4_inversion.sh
@@ -65,8 +65,8 @@ RESTART_FILE="/n/seasasfs02/hnesser/TROPOMI_inversion/restarts/GEOSChem.Restart.
 BC_FILES="/n/seasasfs02/hnesser/TROPOMI_inversion/boundary_conditions/GEOSChem.BoundaryConditions.\$YYYY\$MM\$DD_0000z.nc4"
 
 # Jacobian settings
-nPerturbationsMin=101
-nPerturbationsMax=434
+nPerturbationsMin=11
+nPerturbationsMax=100
 pPERT="1.0E-8"
 
 # Path and file format for eigenvectors 
