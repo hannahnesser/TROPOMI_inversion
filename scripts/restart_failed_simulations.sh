1c8adbbfbbdcc8ed6d8324be60633b68ce17fc0f
Hannah Nesser <hnesser@g.harvard.edu>
2021-10-25 18:16:20 -0400

    Fleshing out

--- scripts/restart_failed_simulations.sh
+++ scripts/restart_failed_simulations.sh
@@ -1,4 +1,6 @@
-for x in $(seq 14 105);
+cd /n/holyscratch01/jacob_lab/hnesser/TROPOMI_inversion/jacobian_runs
+
+for x in $(seq 17 33);
 do
 
 if [ $x -lt 10 ]; then
@@ -11,26 +13,66 @@ else
     xstr="${x}"
 fi
 
-gcdir="TROPOMI_inversion_${xstr}"
+run_name="TROPOMI_inversion_${xstr}"
 
 # Check for GC completion
-GC_check=$(tail -n 2 ${gcdir}/${gcdir}.log | head -n 1)
+GC_check=$(tail -n 2 ${run_name}/${run_name}.log | head -n 1)
 if [[ $GC_check != "**************   E N D   O F   G E O S -- C H E M   **************" ]]
 then
-    echo "GEOS-Chem failed -- ${gcdir}"
+    echo "GEOS-Chem failed -- ${run_name}"
     # All GC runs worked in the last set of perturbations, so we're
     # skipping this part for now
-fi
+else
+    # echo "All GEOS-Chem simulations completed."
+    # Check for completion of preprocessing
+    preprocess_check=$(ls ${run_name}/OutputDir/*_orig | wc -w)
+    if [[ $preprocess_check != 14 ]]; then
+        echo "Preprocessing failed -- ${run_name}"
 
-# Check for pre-processing completion
-preprocess_check=$(tail -n 2 ${gcdir}/preprocess_GC*.out | head -n 1)
+        # Enter the run directory
+        cd ${run_name}
 
-if [[ $preprocess_check != "Replaced data in 14 files." ]]
-then
-  echo "Preprocessing failed -- ${gcdir}"
+        # Modify the run file
+        sed -i -e "s@export OMP_NUM_THREADS@# export OMP_NUM_THREADS@g" \
+               -e "s@. ~/init/init@# . ~/init/init@g" \
+               -e "s@./geos@# ./geos@g" ${run_name}.run
+               # -e "s@##SBATCH@#SBATCH@g" \
+               # -e "s@-c 16@-c 1@g" \
+               # -e "s@-t 4-00:00@-t 0-00:10@g" \
+               # -e "s@--mem=20000@--mem=2000@g" \
 
-  # Turn off switches in the run template
-  sed -i -e "s:. ~/init/init"
+
+        # Remove old output file and resubmit
+        rm preprocess_GC*.out
+        ./${run_name}.run
+
+        # Exit directory
+        cd ..
+    else
+        # echo "All pre-processing completed."
+        # Check for completion of operator
+        operator_check=$(ls ${run_name}/ProcessedDir/ | wc -w)
+        if [[ $operator_check != 365 ]]; then
+            echo "Operator failed -- ${run_name}"
+
+            # Enter the run directory
+            cd ${run_name}
+
+            # Modify the run file
+            sed -i -e "s@export OMP_NUM_THREADS@# export OMP_NUM_THREADS@g" \
+                   -e "s@. ~/init/init@# . ~/init/init@g" \
+                   -e "s@./geos@# ./geos@g" \
+                   -e "s@jid1=@# jid1=@g" \
+                   -e "s@--dependency=afterok:\${jid1##\* } @@g" ${run_name}.run
+
+            # Remove old output files and resubmit
+            rm -f TROPOMI_operator_*
+            ./${run_name}.run
+
+            # Exit directory
+            cd ..
+        fi
+    fi
 fi
 
 done
