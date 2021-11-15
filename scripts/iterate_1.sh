a139b5faeea0a05bef949dc34a5b6a84f157188b
Hannah Nesser <hnesser@g.harvard.edu>
2021-11-14 23:54:49 -0500

    Comment edit

--- scripts/iterate_1.sh
+++ scripts/iterate_1.sh
@@ -13,7 +13,7 @@ jid1=$(sbatch --array=1-12 build_k_monthly.sh ${PRIOR_DIR} ${PERT_DIRS} ${NPERT_
 
 # Calculate the prior preconditioned Hessian
 jid2=$(sbatch --dependency=afterok:${jid1##* } --array=1-12 generate_pph.sh "1" ${SHORT_TERM_DATA_DIR} ${CODE_DIR})
-# sbatch --array=2-12 generate_pph.sh "1" ${SHORT_TERM_DATA_DIR} ${CODE_DIR}
+# sbatch --array=11 generate_pph.sh "1" ${SHORT_TERM_DATA_DIR} ${CODE_DIR}
 
 # Calculate the eigenvectors
 jid3=$(sbatch --dependency=afterok:${jid2##* } generate_evecs.sh "1" ${NUM_EVECS} ${SHORT_TERM_DATA_DIR} ${LONG_TERM_DATA_DIR} ${CODE_DIR})
