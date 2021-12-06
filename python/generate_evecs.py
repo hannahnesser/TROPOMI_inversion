Temporary fix to open npy arrays instead of netcdfs for pre xhat calculation

python/generate_evecs.py
@@ -93,7 +93,10 @@ if __name__ == '__main__':
         for m in s.months:
             print(f'Loading month {m}.')
             temp1 = xr.open_dataarray(f'{data_dir}/pph{niter}_m{m:02d}.nc')
-            temp2 = xr.open_dataarray(f'{data_dir}/pre_xhat{niter}_m{m:02d}.nc')
+            temp2 = np.load(f'{data_dir}/pre_xhat{niter}_m{m:02d}.nc')
+            # temp2 = xr.open_dataarray(f'{data_dir}/pre_xhat{niter}_m{m:02d}.nc')
+            temp2 = xr.DataArray(temp2, dims=['nstate'],
+                                 name=f'pre_xhat{niter}')
             # print(m, temp1.min(), temp1.max())
             pph += temp1
             pre_xhat += temp2