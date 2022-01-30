3d2d3fcd4e9e86dfc6218e5970fbecda18ab1955
Hannah Nesser <hnesser@g.harvard.edu>
2022-01-30 12:09:59 -0500

    Switched RF and sa_scale to be read in by script

--- scripts/solve_inversion.sh
+++ scripts/solve_inversion.sh
@@ -11,6 +11,8 @@ CALCULATE_EVECS="False"
 FORMAT_EVECS="False"
 SOLVE_INVERSION="True"
 CHUNK_SIZE=150000
+RF="0.25"
+SA_SCALE="1"
 
 # Build the Jacobian
 jid1=$(sbatch --array=1-20 build_k_chunks.sh ${CHUNK_SIZE} "2" ${PRIOR_DIR} ${PERT_DIRS} ${NPERT_DIRS} ${SHORT_TERM_DATA_DIR} ${CODE_DIR})
@@ -19,6 +21,6 @@ jid1=$(sbatch --array=1-20 build_k_chunks.sh ${CHUNK_SIZE} "2" ${PRIOR_DIR} ${PE
 jid2=$(sbatch --dependency=afterok:${jid1##* } --array=1-20 generate_pph.sh ${CHUNK_SIZE} "2" ${SHORT_TERM_DATA_DIR} ${CODE_DIR})
 
 # Calculate the eigenvectors
-jid3=$(sbatch --dependency=afterok:${jid2##* } generate_evecs.sh "2" ${NUM_EVECS} ${SHORT_TERM_DATA_DIR} ${LONG_TERM_DATA_DIR} ${CODE_DIR} ${CALCULATE_EVECS} ${FORMAT_EVECS} ${SOLVE_INVERSION})
+jid3=$(sbatch --dependency=afterok:${jid2##* } generate_evecs.sh "2" ${NUM_EVECS} ${SHORT_TERM_DATA_DIR} ${LONG_TERM_DATA_DIR} ${CODE_DIR} ${CALCULATE_EVECS} ${FORMAT_EVECS} ${SOLVE_INVERSION} ${RF} ${SA_SCALE})
 
 
