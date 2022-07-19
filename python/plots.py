d1786f4b32c725f437d1f56a03715e2e87ae9c80
Hannah Nesser <hnesser@g.harvard.edu>
2022-07-19 11:44:41 -0400

    Add Permian prior comparison plot and So comparison plot

--- python/plots.py
+++ python/plots.py
@@ -165,7 +165,7 @@ clusters = xr.open_dataarray(f'{data_dir}clusters.nc')
 #                 'map_kwargs' : small_map_kwargs}
 
 # xhat_cbar_kwargs = {'title' : r'Scale factor'}
-# xhat_kwargs = {'cmap' : 'PuOr_r', 'vmin' : 0.75, 'vmax' : 1.25,
+# xhat_kwargs = {'cmap' : 'PuOr_r', 'vmin' : 0, 'vmax' : 2,
 #                'default_value' : 1,
 #                'cbar_kwargs' : xhat_cbar_kwargs,
 #                'map_kwargs' : small_map_kwargs}
@@ -174,12 +174,13 @@ clusters = xr.open_dataarray(f'{data_dir}clusters.nc')
 # # Fraction of information contents
 # # fracs = [90, 95, 98, 99, 99.9]
 # # fracs = [99.9]
-# suffixes = ['poi50', 'poi90', 'poi95', 'poi99', 'poi99.9']
+# # suffixes = ['poi50', 'poi90', 'poi95', 'poi99', 'poi99.9']
+# suffixes = ['poi90']
 
 # # Load initial averaging kernel
 # for f in suffixes:
-#     dofs = np.load(f'{data_dir}dofs1_{f}.npy')
-#     xhat = np.load(f'{data_dir}xhat1_{f}.npy')
+#     dofs = np.load(f'{data_dir}posterior/archive/dofs1_{f}.npy')
+#     xhat = np.load(f'{data_dir}posterior/archive/xhat1_{f}.npy')
 #     # xhat += np.ones(xhat.shape)
 
 #     # Filter
@@ -187,8 +188,7 @@ clusters = xr.open_dataarray(f'{data_dir}clusters.nc')
 
 #     # Plot averaging kernel sensitivities
 #     title = f'First update of information content' # ({f}\%)'
-#     fig, ax, c = ip.plot_state(dofs, clusters_plot, title=title,
-#                                **avker_kwargs)
+#     fig, ax, c = ip.plot_state(dofs, clusters, title=title, **avker_kwargs)
 #     ax.text(0.025, 0.05, 'DOFS = %d' % round(dofs.sum()),
 #             fontsize=config.LABEL_FONTSIZE*config.SCALE,
 #             transform=ax.transAxes)
@@ -196,8 +196,7 @@ clusters = xr.open_dataarray(f'{data_dir}clusters.nc')
 
 #     # Plot posterior scaling factors
 #     title = f'First update of emissions scale factors' # ({f}\%)'
-#     fig, ax, c = ip.plot_state(xhat, clusters_plot, title=title,
-#                                **xhat_kwargs)
+#     fig, ax, c = ip.plot_state(xhat, clusters, title=title, **xhat_kwargs)
 #     fp.save_fig(fig, plot_dir, f'fig_est1_xhat_{f}_update')
 
 #     # Reset cbar kwargs
@@ -361,27 +360,130 @@ clusters = xr.open_dataarray(f'{data_dir}clusters.nc')
 #                                              'ticks' : [-0.01, 0, 0.01]})
 # fp.save_fig(fig, plot_dir, f'fig_est1_evecs_{n0}_{n0+nevecs}')
 
-## ------------------------------------------------------------------------ ##
-## Figure: Error distribution comparison
-## ------------------------------------------------------------------------ ##
-labels = ['Native', '1x1', '2x2', '3x3', '4x4']
-fig, ax = fig, ax = fp.get_figax(rows=2, cols=1, aspect=1.75, sharex=True)
-for i, grid in enumerate(['native', 'rg1', 'rg2', 'rg3', 'rg4']):
-    so = pd.Series(xr.open_dataarray(f'{data_dir}/so_{grid}.nc')**0.5)
-    so.plot(ax=ax[0], kind='kde', color=fp.color(2*i),
-                # histtype='step', bins=50, density=True,
-                label=labels[i])
+# ------------------------------------------------------------------------ ##
+# Figure: Error distribution comparison
+# ------------------------------------------------------------------------ ##
+# labels = ['Native', '1x1', '2x2', '3x3', '4x4']
+# fig, ax = fig, ax = fp.get_figax(rows=1, cols=2, aspect=1.75, sharey=True)
+# for i, grid in enumerate(['native', 'rg1', 'rg2', 'rg3', 'rg4']):
+#     so = pd.Series(xr.open_dataarray(f'{data_dir}/so_{grid}.nc')**0.5)
+#     so.plot(ax=ax[0], kind='kde', color=fp.color(2*i))
+#                 # histtype='step', bins=50, density=True,
+#                 # label=labels[i])
 
-    so_rt = pd.Series(xr.open_dataarray(f'{data_dir}/so_{grid}rt.nc')**0.5)
-    so_rt.plot(ax=ax[1], kind='kde', color=fp.color(2*i),
-                   # histtype='step', bins=50, density=True,
-               label=f'{labels[i]} seasonal')
+#     so_rt = pd.Series(xr.open_dataarray(f'{data_dir}/so_{grid}rt.nc')**0.5)
+#     so_rt.plot(ax=ax[1], kind='kde', color=fp.color(2*i), label=labels[i])
+#                    # histtype='step', bins=50, density=True,
+#                # label=f'{labels[i]} seasonal')
 
-ax[0] = fp.add_title(ax[0], 'Observational Error')
-ax[0].set_xlim(0, 25)
-ax[1].set_xlim(0, 25)
-ax[0]= fp.add_labels(ax[0], '', 'Count')
-ax[1]= fp.add_labels(ax[1], 'Observational Error (ppb)', 'Count')
-ax[0] = fp.add_legend(ax[0])
-ax[1] = fp.add_legend(ax[1])
-fp.save_fig(fig, plot_dir, f'observational_error_summary')
+# # Add in threshold
+# so_rt = pd.Series(xr.open_dataarray(f'{data_dir}/so_rg2rt_10t.nc')**0.5)
+# so_rt.plot(ax=ax[1], kind='kde', color=fp.color(2*(i+1)), label='2x2, threshold')
+
+
+# ax[0] = fp.add_title(ax[0], 'Monthly averaged bias')
+# ax[1] = fp.add_title(ax[1], 'Seasonally averaged bias')
+# ax[0].set_xlim(0, 25)
+# ax[1].set_xlim(0, 25)
+# ax[0]= fp.add_labels(ax[0], 'Observational Error (ppb)', 'Count')
+# ax[1]= fp.add_labels(ax[1], 'Observational Error (ppb)', '')
+# ax[1] = fp.add_legend(ax[1], bbox_to_anchor=(1, 0.5), loc='center left', ncol=1)
+
+# fp.save_fig(fig, plot_dir, f'observational_error_summary')
+
+# ------------------------------------------------------------------------ ##
+# Figure: Permian prior and observation comparison
+# ------------------------------------------------------------------------ ##
+# Open prior and clusterr files
+# xa_abs = xr.open_dataarray(f'{data_dir}xa_abs_bc0.nc') # Mg/km2/yr --> kg/km2/hr
+xa_abs = xr.open_dataarray(f'{data_dir}xa_abs_correct.nc')#*1000/365/24
+xa_abs_edf = xr.open_dataarray(f'{data_dir}xa_abs_edf.nc')#*1000/365/24
+permian_idx = np.load(f'{data_dir}permian_idx.npy')
+xa_abs_permian = xa_abs#[permian_idx]
+xa_abs_edf_permian = xa_abs_edf#[permian_idx]
+
+# Get the Permian cluster and basin indices (discard the buffer cells)
+permian = xr.open_dataset(f'{data_dir}clusters_permian.nc')['Clusters']
+c = clusters.squeeze(drop=True).to_dataset()
+c['Permian'] = permian
+cell_idx, cell_cnt  = np.unique(c['Permian'], return_counts=True)
+cell_idx = cell_idx[cell_cnt == 1]
+cell_idx = cell_idx[~np.isnan(cell_idx)]
+permian = permian.where(permian.isin(cell_idx), 0)
+p_lats = permian.where(permian > 0, drop=True).lat.values
+p_lats = [p_lats[0] - 2, p_lats[-1] + 2]
+p_lons = permian.where(permian > 0, drop=True).lon.values
+p_lons = [p_lons[0] - 2, p_lons[-1] + 2]
+print('-'*30)
+print(p_lats)
+print(p_lons)
+print('-'*30)
+
+# Colormap
+yor_trans = fp.cmap_trans('YlOrRd', nalpha=100)
+
+# Make figure
+fig, ax = fp.get_figax(rows=1, cols=3, maps=True, lats=p_lats, lons=p_lons)
+# plt.subplots_adjust(wspace=1)
+
+# Prior (EPA)
+fig_kwargs = {'figax' : [fig, ax[0]]}
+cbar_kwargs = {'title' : 'Emissions\n' r'(Mg km$^{-2}$ a$^{-1}$)',
+               'horizontal' : False}
+obs_kw = {'cmap' : viridis_trans, 'vmin' : 0, 'vmax' : 20, 'default_value' : 0,
+          'cbar' : False, 'map_kwargs' : small_map_kwargs,
+          'fig_kwargs' : fig_kwargs}
+title = 'Initial emissions\nestimate' # ({f}\%)'
+fig, ax[0], c = ip.plot_state(xa_abs_permian, clusters, title=title, **obs_kw)
+ax[0] = fp.format_map(ax[0], p_lats, p_lons, **small_map_kwargs)
+# ax[0, 0].text(0.05, 0.05, f'{(xa_abs_permian.sum().values):.1f} Tg/yr',
+#                 fontsize=config.LABEL_FONTSIZE*config.SCALE*0.75,
+#                 transform=ax[0, 0].transAxes)
+
+# Prior (EDF)
+fig_kwargs = {'figax' : [fig, ax[1]]}
+cbar_kwargs = {'title' : 'Emissions\n' r'(kg km$^2$ hr$^{-1}$)',
+               'horizontal' : True}
+obs_kw = {'cmap' : viridis_trans, 'vmin' : 0, 'vmax' : 20, 'default_value' : 0,
+          'cbar' : False, 'map_kwargs' : small_map_kwargs,
+          'fig_kwargs' : fig_kwargs}
+obs_kw['fig_kwargs'] = {'figax' : [fig, ax[1]]}
+
+title = f'High-resolution\ninventory' # ({f}\%)'
+fig, ax[1], c = ip.plot_state(xa_abs_edf_permian, clusters,
+                              title=title, **obs_kw)
+ax[1] = fp.format_map(ax[1], p_lats, p_lons, **small_map_kwargs)
+
+# Add cbar for ax 0 and 1
+cax = fp.add_cax(fig, ax[0:2], horizontal=True)
+cb = fig.colorbar(c, cax=cax, ax=ax[0:2], orientation='horizontal')
+cb = fp.format_cbar(cb, cbar_kwargs['title'], horizontal=True)
+
+# Observations
+lat, lon = gc.create_gc_grid(permian.lat.min(), permian.lat.max(), s.lat_delta,
+                             permian.lon.min(), permian.lon.max(), s.lon_delta,
+                             centers=False, return_xarray=False)
+
+obs = gc.read_file(join(data_dir, f'{s.year}_corrected.pkl'))
+
+# Select only one month
+obs = obs[obs['MONTH'] == 3]
+
+# Save nearest latitude and longitude centers
+lat_idx = gc.nearest_loc(obs['LAT'].values, lat)
+obs.loc[:, 'LAT_CENTER'] = lat[lat_idx]
+lon_idx = gc.nearest_loc(obs['LON'].values, lon)
+obs.loc[:, 'LON_CENTER'] = lon[lon_idx]
+
+groupby = ['LAT_CENTER', 'LON_CENTER']
+obs = obs.groupby(groupby).mean()['OBS'].to_xarray()
+obs = obs.rename({'LAT_CENTER' : 'lats', 'LON_CENTER' : 'lons'})
+
+ax[-1] = fp.format_map(ax[-1], p_lats, p_lons, **small_map_kwargs)
+c = obs.plot(ax=ax[-1], cmap='plasma', vmin=1800, vmax=1875, add_colorbar=False)
+cax = fp.add_cax(fig, ax[-1], horizontal=True)
+cb = fig.colorbar(c, ax=ax[-1], cax=cax, orientation='horizontal')
+cb = fp.format_cbar(cb, 'Column mixing ratio\n(ppb)', horizontal=True)
+ax[-1] = fp.add_title(ax[-1], 'TROPOMI\nmethane')
+
+fp.save_fig(fig, plot_dir, f'fig_permian_prior_v_obs')
