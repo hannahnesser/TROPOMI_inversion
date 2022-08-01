e7cfbf736beff73cb8c166dcca1efd801a7640e9
Hannah Nesser <hnesser@g.harvard.edu>
2022-08-01 14:17:30 -0400

    Bug fix

--- python/solve_inversion.py
+++ python/solve_inversion.py
@@ -281,7 +281,7 @@ if __name__ == '__main__':
                 # # be pertinent when we do our posterior model comparaison
 
                 # Save out
-                suff = suffix + f'_rf{rf_i}' + f'_sax{sa_scale}' + f'_poi{pct_of_info}'
+                suff = suffix + f'_rf{rf_i}' + f'_sax{sa_i}' + f'_poi{pct_of_info}'
                 np.save(f'{data_dir}/iteration{niter}/a/dofs{niter}{suff}.npy', dofs)
                 np.save(f'{data_dir}/iteration{niter}/xhat/xhat{niter}{suff}.npy', xh_fr)
                 # np.save(f'{data_dir}/iteration{niter}/y/y{niter}{suff}.npy', yhat)
