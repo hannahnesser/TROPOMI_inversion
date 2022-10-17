6be7297d0f6e682aae4df6601829b35fdda10531
Hannah Nesser <hnesser@g.harvard.edu>
2022-10-16 23:16:09 -0400

    Reverting to the old A calculation

--- python/invpy.py
+++ python/invpy.py
@@ -461,18 +461,18 @@ def pph(k, so, sa, big_mem=False):
 ## -------------------------------------------------------------------------##
 ## Source attribution functions
 ## -------------------------------------------------------------------------##
-def source_attribution(w, xhat, shat=None, a=None, sa=None):
+def source_attribution(w, xhat, shat=None, a=None):
     # xhat red = W xhat
     xhat_red = w @ xhat
 
-    if (shat is not None) and (a is not None) and (sa is not None):
+    if (shat is not None) and (a is not None):
         # Convert error covariance matrices
         # S red = W S W^T
         shat_red = w @ shat @ w.T
-        sa_red = w @ (sa*w.T)
 
         # Calculate reduced averaging kernel
-        a_red = np.identity(w.shape[0]) - shat_red @ inv(sa_red)
+        # a_red = np.identity(w.shape[0]) - shat_red @ inv(sa_red)
+        a_red = w @ a @ w.T @ inv((w @ w.T))
         return xhat_red, shat_red, a_red
     else:
         return xhat_red
