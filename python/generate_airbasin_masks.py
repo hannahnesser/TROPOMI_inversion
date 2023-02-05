038f1468f3b50ce8322f4d43a69d4a7e1b92f764
Hannah Nesser <hnesser@g.harvard.edu>
2023-02-05 11:49:28 -0500

    Adding a few mask generation files

--- python/generate_airbasin_masks.py
+++ python/generate_airbasin_masks.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import shapefile
 from shapely.geometry import Polygon, MultiPolygon
+from pyproj import Proj, transform
 pd.set_option('display.max_columns', 10)
 
 # Custom packages
@@ -27,26 +28,21 @@ clusters = xr.open_dataarray(f'{data_dir}clusters.nc').squeeze()
 nstate = int(clusters.values.max())
 
 # Open populations
-pop = pd.read_csv(f'{data_dir}cities/urban_areas_pop.csv', header=0)
+# pop = pd.read_csv(f'{data_dir}cities/urban_areas_pop.csv', header=0)
 
-# Open the 2019 city boundaries
-city = shapefile.Reader(f'{data_dir}cities/2019_tl_urban_areas/tl_2019_us_uac10_buffered.shp')
+# Open the air basin boundaries
+airbasin = shapefile.Reader(f'{data_dir}states/ca_air_basins/CaAirBasin_WGS84_R2.shp')
 
 # Create storage for the masks
-w_city = pd.DataFrame(
-             columns=[s.record[2] for s in city.shapeRecords()
-                      if (s.record[3][-4:] == 'Area') and 
-                      (s.record[2] in pop['Name'].values) and
-                      (s.record[2].split(', ')[-1] not in ['AK', 'HI', 'PR'])])
+w_ca = pd.DataFrame(columns=[s.record[4] for s in airbasin.shapeRecords()])
 
-# Iterate through each city
-for j, shape in enumerate(city.shapeRecords()):
-    if shape.record[2] in w_city.columns:
-        # Add a column to w_city
-        x = [i[0] for i in shape.shape.points[:]]
-        y = [i[1] for i in shape.shape.points[:]]
-        w_city[shape.record[2]] = gc.grid_shape_overlap(clusters, x, y, 
-                                                        shape.record[2])
+# Iterate through each airbasin
+for j, shape in enumerate(airbasin.shapeRecords()):
+    # Add a column to w_city
+    x = [i[0] for i in shape.shape.points[:]]
+    y = [i[1] for i in shape.shape.points[:]]
+    w_ca[shape.record[4]] = gc.grid_shape_overlap(clusters, x, y, 
+                                                    shape.record[4])
 
 # Save out w_city
-w_city.to_csv(f'{data_dir}cities/urban_areas_mask.csv', header=True, index=False)
+w_ca.to_csv(f'{data_dir}states/airbasin_mask.csv', header=True, index=False)
