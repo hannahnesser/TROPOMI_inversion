681aed02e037a20789fcc1f487cccd5eae74acc8
Hannah Nesser <hnesser@g.harvard.edu>
2023-04-29 18:01:53 -0400

    Adding default sectoral information

--- python/inversion_settings.py
+++ python/inversion_settings.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('.')
 import gcpy as gc
+import format_plots as fp
 
 # Time settings
 year = 2019
@@ -26,3 +27,49 @@ lon_max = lon_max - lon_delta*buffers[2] + lon_delta/2
 
 lats = [lat_min, lat_max]
 lons = [lon_min, lon_max]
+
+# Sectors
+sector_groups = {'wetlands' : 'Wetlands',
+                 'livestock' : 'Livestock',
+                 'coal' : 'Coal',
+                 'ong' : ['Oil', 'Gas'],
+                 'landfills' : 'Landfills',
+                 'wastewater' : 'Wastewater',
+                 'other_anth' : ['Rice', 'OtherAnth'],
+                 'other_bio' : ['Termites', 'Seeps', 'BiomassBurn', 'Lakes']}
+
+# High resolution sectoral breakdown
+sector_groups_hr = {'enteric_fermentation' : 'LIVESTOCK_4A',
+                    'manure_management' : 'LIVESTOCK_4B',
+                    'coal_abandoned' : 'COAL_ABANDONED',
+                    'coal_surface' : 'COAL_SURFACE',
+                    'coal_underground' : 'COAL_UNDERGROUND',
+                    'ong_upstream' : ['OIL', 'GAS_PRODUCTION', 
+                                      'GAS_PROCESSING', 'GAS_TRANSMISSION'],
+                    'gas_distribution' : 'GAS_DISTRIBUTION',
+                    'landfills' : ['LANDFILLS_IND', 'LANDFILLS_MUN'],
+                    'wastewater' : ['WASTEWATER_IND', 'WASTEWATER_DOM'],
+                    'other_anth' : ['OTHERANTH_1A_M', 'OTHERANTH_1A_S',
+                                    'OTHERANTH_2B5', 'OTHERANTH_2C2', 
+                                    'OTHERANTH_4F', 'OTHERANTH_5',
+                                    'OTHERANTH_6D']}
+
+# Sector naames
+sectors = {'Total'               : 'total', 
+           'Livestock'           : 'livestock', 
+           'Oil and gas'         : 'ong',
+           'Coal'                : 'coal', 
+           'Landfills'           : 'landfills',
+           'Wastewater'          : 'wastewater', 
+           'Other anthropogenic' : 'other_anth',
+           'Wetlands'            : 'wetlands',
+           'Other biogenic'      : 'other_bio'}
+
+# Sectoral colors
+sector_colors = {'livestock'  : fp.color(4, cmap='plasma', lut=17),
+                 'ong'        : fp.color(0, cmap='plasma', lut=17),
+                 'coal'       : fp.color(11, cmap='plasma', lut=17),
+                 'landfills'  : fp.color(8, cmap='plasma', lut=17),
+                 'wastewater' : fp.color(15, cmap='plasma', lut=17),
+                 'other_anth' : fp.color(13, cmap='viridis', lut=17),
+                 'wetlands'   : '0.5'}
